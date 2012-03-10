c363c1b28c08d751d19e8f7c7f2400f62f9aa389
unknown <Dusan@Dusan-NB.(none)>
2012-03-10 21:06:34 +0100

    web frontend initial project structure

---
+++ webfrontend/src/main/java/cz/cuni/mff/odcleanstore/webfrontend/WicketApplication.java
@@ -0,0 +1,24 @@
+package cz.cuni.mff.odcleanstore.webfrontend;
+
+import org.apache.wicket.protocol.http.WebApplication;
+
+/**
+ * Web Frontend Application object.
+ * 
+ */
+public class WicketApplication extends WebApplication
+{
+	@Override
+	public Class<HomePage> getHomePage()
+	{
+		return HomePage.class;
+	}
+
+	@Override
+	public void init()
+	{
+		super.init();
+
+		// configuration is to be added here
+	}
+}
