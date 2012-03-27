3c7600327da9ec43d2c6563ab5563df5ec0473b9
Petr Jerman <petr.jerman@centrum.cz>
2012-03-27 23:22:56 +0200

    Engine-prototype EngineSecurityManager changed

--- odcleanstore/engine/src/main/java/cz/cuni/mff/odcleanstore/engine/core/Empty.java
+++ odcleanstore/engine/src/main/java/cz/cuni/mff/odcleanstore/engine/core/Empty.java
@@ -10,13 +10,16 @@ import java.util.Iterator;
  * @author Petr Jerman <petr.jerman@centrum.cz>
  */
 public class Empty {
+
 	public static final Iterable<String> ITERABLE_STRING = new Iterable<String>() {
+
 		public Iterator<String> iterator() {
 			return ITERATOR_STRING;
 		}
 	};
 
 	private static final Iterator<String> ITERATOR_STRING = new Iterator<String>() {
+
 		public boolean hasNext() {
 			return false;
 		}
