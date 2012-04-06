383ac8e84c93b0400c5cd50773ea7742313235ce
Jan Michelfeit <michelfeit.jan@gmail.com>
2012-04-06 20:43:01 +0200

    Transformer interface prototype

--- prototype/interfaces/transformer/TransformedGraph.java
+++ prototype/interfaces/transformer/TransformedGraph.java
@@ -1,23 +1,67 @@
-package cz.cuni.mff.odcleanstore.test;
-
-import com.hp.hpl.jena.rdf.model.Model;
+package cz.cuni.mff.odcleanstore.transformer;
 
 import java.util.Collection;
 
 /**
  * Container for all neccessary information about a transofmed graph for a custom transformer.
  *
- * @todo just a prototype
  * @author Jan Michelfeit
  */
-public abstract class TransformedGraph {
-    public abstract Model getModel();
+public interface TransformedGraph {
+    /**
+     * TODO
+     * Returns an editable model of the transformed graph.
+     * (jenom pokud to pujde udelat)
+     */
+    //Model getModel();
 
-    public abstract String getGraphName(); // URI jmenneho graf
+    /**
+     * Returns URI of the transformed named graph.
+     * @return named graph URI
+     */
+    String getGraphName();
 
-    public abstract String getGraphId(); // unikatni ID grafu, aby ho v pripade potreby nebylo nutne tahat z URI grafu
+    /**
+     * Returns unique identifier in ODCS of the transform named graph.
+     * @return id string
+     */
+    String getGraphId();
 
-    public abstract Model getMetadata(); // provenance (a dalsi) metadata
+    /**
+     * Returns URI of the named graph containing (provenance) metadata about the transformed graph.
+     * @return named graph URI
+     */
+    String getMetadataGraphName();
 
-    public abstract Collection<String> getSources(); // mozna zpristupnit nektera metadata i explicitne?
+    /**
+     * Returns a model of the named graph containing (provenance) metadata about the transformed
+     * graph.
+     */
+    //Model getMetadataModel();
+
+    /**
+     * Returns a collection of graphs attached to the transformed graph registered by
+     * {@link #addAttachedGraph(String)} method.
+     * Contents of the attached graphs may be changed by the Transformer.
+     * @return collection of named graphs' URIs
+     */
+    Collection<String> getAttachedGraphNames();
+
+    /**
+     * Register an additional graph attached to the main transformed graph.
+     * @param attachedGraphName URI of the attached named graph
+     */
+    void addAttachedGraph(String attachedGraphName);
+
+    /**
+     * Marks the whole transformed graph (and the respective attached graphs) for deletion.
+     */
+    void deleteGraph();
+
+    /**
+     * Returns true iff the transformed graph has been marked for deletion.
+     * @see #deleteGraph()
+     * @return true iff the transformed graph has been marked for deletion
+     */
+    boolean isDeleted();
 }
--- prototype/interfaces/transformer/Transformer.java
+++ prototype/interfaces/transformer/Transformer.java
@@ -1,15 +1,28 @@
-package cz.cuni.mff.odcleanstore.test;
-
-import com.hp.hpl.jena.rdf.model.Model;
+package cz.cuni.mff.odcleanstore.transformer;
 
 /**
  * Interface of a custom transformer.
  *
- * @todo just a prototype
- * @author Jan Michelfeit
+ * The transformer can modify the given input named graph, attach a new named graph to the
+ * transformed graph (see {@link TransformedGraph#addAttachedGraph(String)}) or modify any of the
+ * already attached named graphs.
+ * The caller must ensure that the transformed named graphs (and the respective attached graphs) are
+ * not modified while the Transformer is working on it.
  *
+ * @author Jan Michelfeit
  */
 public interface Transformer {
-    // Na vystupu muze byt bud upraveny inputGraph.getModel() nebo uplne novy Model
-    public Model transform(TransformedGraph inputGraph, TransformationContext context);
+    /**
+     * Transforms a new graph in the dirty database.
+     * @param inputGraph holder for the transformed graph.
+     * @param context context of the transformation
+     */
+    void transformNewGraph(TransformedGraph inputGraph, TransformationContext context);
+
+    /**
+     * Transforms an existing graph in the clean database.
+     * @param inputGraph holder for the transformed graph.
+     * @param context context of the transformation
+     */
+    void transformExistingGraph(TransformedGraph inputGraph, TransformationContext context);
 }
