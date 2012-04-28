6c716171f982a0edf03571885f584c21e400a444
Petr Jerman <petr.jerman@centrum.cz>
2012-04-28 20:02:38 +0200

    Backend - Transformer interface - TransformedGraphException added and TransformerGraph throws changed

---
+++ odcleanstore/backend/src/main/java/cz/cuni/mff/odcleanstore/transformer/TransformedGraphException.java
@@ -0,0 +1,37 @@
+/**
+ *
+ */
+package cz.cuni.mff.odcleanstore.transformer;
+
+import cz.cuni.mff.odcleanstore.shared.ODCleanStoreException;
+
+/**
+ * Exception thrown by TransformedGraphs during a named graph processing.
+ * @author Petr Jerman
+ */
+public class TransformedGraphException extends ODCleanStoreException {
+    /**
+     * Constructs a new exception with the given cause.
+     * @param cause the cause
+     */
+    public TransformedGraphException(Throwable cause) {
+        super(cause);
+    }
+
+    /**
+     * Constructs a new exception with the given message and cause.
+     * @param message the detail message
+     * @param cause the cause
+     */
+    public TransformedGraphException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * Constructs a new exception with the given message.
+     * @param message the detail message
+     */
+    public TransformedGraphException(String message) {
+        super(message);
+    }
+}
