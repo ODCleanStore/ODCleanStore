63b082c43490046059388447761c227b0e9a8915
unknown <Dusan@Dusan-NTB.(none)>
2012-05-12 16:24:52 +0200

    Moved the initial DB import script from BitBucket to SourceForge.

---
+++ initial_db_import/schema_and_initial_data.sql
@@ -0,0 +1,184 @@
+DROP TABLE DB.FRONTEND.ROLES_ASSIGNED_TO_USERS;
+DROP TABLE DB.FRONTEND.USERS;
+DROP TABLE DB.FRONTEND.ROLES;
+
+DROP TABLE DB.FRONTEND.REGISTERED_TRANSFORMERS;
+DROP TABLE DB.FRONTEND.EN_INPUT_GRAPHS;
+DROP TABLE DB.FRONTEND.EN_WORKING_ADDED_GRAPHS;
+
+DROP TABLE DB.FRONTEND.EL_RULES_TO_DOMAINS_RESTRICTIONS;
+DROP TABLE DB.FRONTEND.EL_RULES;
+DROP TABLE DB.FRONTEND.DATA_DOMAINS;
+
+DROP TABLE DB.FRONTEND.OI_RULES;
+DROP TABLE DB.FRONTEND.OI_RULES_GROUPS;
+
+DROP TABLE DB.FRONTEND.CR_SETTINGS;
+DROP TABLE DB.FRONTEND.CR_PROPERTY_AGGREGATIONS;
+DROP TABLE DB.FRONTEND.CR_AGGREGATION_TYPES;
+DROP TABLE DB.FRONTEND.CR_PROPERTIES;
+
+/*
+	===========================================================================
+	USERS MANAGEMENT
+	===========================================================================
+*/
+CREATE TABLE DB.FRONTEND.USERS
+(
+	id INTEGER NOT NULL IDENTITY PRIMARY KEY,
+	username NVARCHAR(255) NOT NULL UNIQUE,
+	email NVARCHAR(255) NOT NULL UNIQUE,
+	passwordHash NVARCHAR(255) NOT NULL,
+	salt NVARCHAR(255) NOT NULL,
+	firstname NVARCHAR(255) NOT NULL,
+	surname NVARCHAR(255) NOT NULL
+);
+
+INSERT INTO DB.FRONTEND.USERS (username, email, passwordHash, salt, firstname, surname) 
+VALUES ('dusanr', 'dusanr@odcleanstore.cz', 'f33b354b1a67af018bf7725049ad1036', 'salted', 'Dusan', 'Rychnovsky');
+
+INSERT INTO DB.FRONTEND.USERS (username, email, passwordHash, salt, firstname, surname) 
+VALUES ('jakubd', 'jakubd@odcleanstore.cz', '669f023337b5b15eed1b3ca8400f4ef1', 'salted', 'Jakub', 'Daniel');
+
+CREATE TABLE DB.FRONTEND.ROLES
+(
+	id INTEGER NOT NULL IDENTITY PRIMARY KEY,
+	label NVARCHAR(255) NOT NULL,
+	description LONG NVARCHAR
+);
+
+INSERT INTO DB.FRONTEND.ROLES (label, description) VALUES ('SCR', 'Scrapper');
+INSERT INTO DB.FRONTEND.ROLES (label, description) VALUES ('ONC', 'Ontology creator');
+INSERT INTO DB.FRONTEND.ROLES (label, description) VALUES ('POC', 'Policy creator');
+INSERT INTO DB.FRONTEND.ROLES (label, description) VALUES ('ADM', 'Administrator');
+
+CREATE TABLE DB.FRONTEND.ROLES_ASSIGNED_TO_USERS
+(
+	userId INTEGER NOT NULL,
+	roleId INTEGER NOT NULL,
+	
+	PRIMARY KEY (userId, roleId),
+	FOREIGN KEY (userId) REFERENCES DB.FRONTEND.USERS(id),
+	FOREIGN KEY (roleId) REFERENCES DB.FRONTEND.ROLES(id)
+);
+
+INSERT INTO DB.FRONTEND.ROLES_ASSIGNED_TO_USERS VALUES (1, 4);
+INSERT INTO DB.FRONTEND.ROLES_ASSIGNED_TO_USERS VALUES (2, 2);
+INSERT INTO DB.FRONTEND.ROLES_ASSIGNED_TO_USERS VALUES (2, 3);
+
+/*
+	===========================================================================
+	ENGINE CONFIGURATION
+	===========================================================================
+*/
+CREATE TABLE DB.FRONTEND.REGISTERED_TRANSFORMERS
+(
+	id INTEGER NOT NULL IDENTITY PRIMARY KEY,
+	label NVARCHAR(255) NOT NULL,
+	description LONG NVARCHAR,
+	jarPath NVARCHAR(255) NOT NULL,
+	fullClassName NVARCHAR(255) NOT NULL,
+	workDirPath NVARCHAR(255) NOT NULL,
+	configuration LONG NVARCHAR NOT NULL,
+	active SMALLINT NOT NULL DEFAULT 0,
+	priority INTEGER NOT NULL DEFAULT 0
+);
+
+/* a temporary table (to be replaced later, not included in the diagram) */
+CREATE TABLE DB.FRONTEND.EN_INPUT_GRAPHS
+(
+	uuid VARCHAR(48) PRIMARY KEY,
+	state VARCHAR(16) NOT NULL
+);
+
+/* a temporary table (to be replaced later, not included in the diagram) */
+CREATE TABLE DB.FRONTEND.EN_WORKING_ADDED_GRAPHS
+(
+	name NVARCHAR PRIMARY KEY
+);
+
+/*
+	===========================================================================
+	EL RULES MANAGEMENT
+	===========================================================================
+*/
+CREATE TABLE DB.FRONTEND.EL_RULES
+(
+	id INTEGER NOT NULL IDENTITY PRIMARY KEY,
+	filter LONG NVARCHAR NOT NULL,
+	coeficient REAL NOT NULL,
+	description LONG NVARCHAR
+);
+
+CREATE TABLE DB.FRONTEND.DATA_DOMAINS
+(
+	id INTEGER NOT NULL IDENTITY PRIMARY KEY,
+	uri NVARCHAR(1024) NOT NULL
+);
+
+CREATE TABLE DB.FRONTEND.EL_RULES_TO_DOMAINS_RESTRICTIONS
+(
+	ruleId INTEGER NOT NULL,
+	domainId INTEGER NOT NULL,
+	
+	PRIMARY KEY (ruleId, domainId),
+	FOREIGN KEY (ruleId) REFERENCES DB.FRONTEND.EL_RULES(id),
+	FOREIGN KEY (domainId) REFERENCES DB.FRONTEND.DATA_DOMAINS(id)
+);
+
+/*
+	===========================================================================
+	OI RULES MANAGEMENT
+	===========================================================================
+*/
+CREATE TABLE DB.FRONTEND.OI_RULES_GROUPS
+(
+	id INTEGER NOT NULL IDENTITY PRIMARY KEY,
+	label NVARCHAR(255) NOT NULL,
+	description LONG NVARCHAR
+);
+
+CREATE TABLE DB.FRONTEND.OI_RULES
+(
+	id INTEGER NOT NULL IDENTITY PRIMARY KEY,
+	groupId INTEGER NOT NULL,
+	definition LONG NVARCHAR NOT NULL,
+	
+	FOREIGN KEY (groupId) REFERENCES DB.FRONTEND.OI_RULES_GROUPS(id)
+);
+
+/*
+	===========================================================================
+	CR RULES MANAGEMENT
+	===========================================================================
+*/
+CREATE TABLE DB.FRONTEND.CR_SETTINGS
+(
+	id INTEGER NOT NULL IDENTITY PRIMARY KEY,
+	name NVARCHAR(255) NOT NULL,
+	value NVARCHAR(255) NOT NULL,
+	description LONG NVARCHAR
+);
+
+CREATE TABLE DB.FRONTEND.CR_AGGREGATION_TYPES
+(
+	id INTEGER NOT NULL IDENTITY PRIMARY KEY,
+	label NVARCHAR(255) UNIQUE NOT NULL,
+	description LONG NVARCHAR	
+);
+
+CREATE TABLE DB.FRONTEND.CR_PROPERTIES
+(
+	id INTEGER NOT NULL IDENTITY PRIMARY KEY,
+	property NVARCHAR(1024) UNIQUE NOT NULL,
+	multivalue SMALLINT NOT NULL DEFAULT 1
+);
+
+CREATE TABLE DB.FRONTEND.CR_PROPERTY_AGGREGATIONS
+(
+	propertyId INTEGER NOT NULL PRIMARY KEY,
+	aggregationTypeId INTEGER NOT NULL,
+
+	FOREIGN KEY (propertyId) REFERENCES DB.FRONTEND.CR_PROPERTIES(id),
+	FOREIGN KEY (aggregationTypeId) REFERENCES DB.FRONTEND.CR_AGGREGATION_TYPES(id)
+);
