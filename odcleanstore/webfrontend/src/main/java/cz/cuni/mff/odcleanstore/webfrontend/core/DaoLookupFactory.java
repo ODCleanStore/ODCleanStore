53b01c096da200b5a1b2b3e88bc1f26c8f9a8e21
Dusan Rychnovsky <dusan.rychnovsky@gmail.com>
2012-05-30 08:32:03 +0200

    WEB FRONTEND - removed dependency on Java 1.7 (e.g. the use of ReflectiveOperationException)

--- odcleanstore/webfrontend/src/main/java/cz/cuni/mff/odcleanstore/webfrontend/core/DaoLookupFactory.java
+++ odcleanstore/webfrontend/src/main/java/cz/cuni/mff/odcleanstore/webfrontend/core/DaoLookupFactory.java
@@ -66,7 +66,7 @@ public class DaoLookupFactory
 		try {
 			daoInstance = daoClass.newInstance();
 		} 
-		catch (ReflectiveOperationException ex) 
+		catch (Exception ex) 
 		{
 			throw new AssertionError(
 				"Could not load DAO class: " + daoClass
