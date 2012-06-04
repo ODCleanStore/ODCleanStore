d8c8f5ff62fcd4a963e71243be1d199eb384bb79
Dusan Rychnovsky <dusan.rychnovsky@gmail.com>
2012-06-04 09:39:59 +0200

    DB SCHEMA - add descriptions to transformer rows in the test-engine init script

--- data/initial_db_import/clean_db/test_data_Engine.sql
+++ data/initial_db_import/clean_db/test_data_Engine.sql
@@ -1,7 +1,7 @@
 DELETE FROM DB.ODCLEANSTORE.TRANSFORMERS;
 
-INSERT INTO DB.ODCLEANSTORE.TRANSFORMERS (label, jarPath, fullClassName) 
-VALUES (n'QA', n'.', n'cz.cuni.mff.odcleanstore.qualityassessment.impl.QualityAssessorImpl');
+INSERT INTO DB.ODCLEANSTORE.TRANSFORMERS (label, description, jarPath, fullClassName) 
+VALUES (n'QA', n'The standard quality assessment transformer', n'.', n'cz.cuni.mff.odcleanstore.qualityassessment.impl.QualityAssessorImpl');
 
-INSERT INTO DB.ODCLEANSTORE.TRANSFORMERS (label, jarPath, fullClassName) 
-VALUES (n'Linker', n'.', n'cz.cuni.mff.odcleanstore.linker.impl.LinkerImpl');
+INSERT INTO DB.ODCLEANSTORE.TRANSFORMERS (label, description, jarPath, fullClassName) 
+VALUES (n'Linker', n'The standard object identification transformer',  n'.', n'cz.cuni.mff.odcleanstore.linker.impl.LinkerImpl');
