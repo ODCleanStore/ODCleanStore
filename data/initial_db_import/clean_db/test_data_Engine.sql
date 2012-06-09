4409ee2c1f5cbb3f79a8e4ac8b102c4c9567760b
Petr Jerman <petr.jerman@centrum.cz>
2012-06-09 19:30:18 +0200

    Initial script for test_data_Engine changed

--- data/initial_db_import/clean_db/test_data_Engine.sql
+++ data/initial_db_import/clean_db/test_data_Engine.sql
@@ -1,4 +1,10 @@
+DELETE FROM DB.ODCLEANSTORE.TRANSFORMERS_TO_PIPELINES_ASSIGNMENT;
 DELETE FROM DB.ODCLEANSTORE.TRANSFORMERS;
+DELETE FROM DB.ODCLEANSTORE.PIPELINES;
+
+set_identity_column('DB.ODCLEANSTORE.TRANSFORMERS','id', 1);
+set_identity_column('DB.ODCLEANSTORE.PIPELINES','id', 1);
+
 
 INSERT INTO DB.ODCLEANSTORE.TRANSFORMERS (label, description, jarPath, fullClassName) 
 VALUES (n'QA', n'The standard quality assessment transformer', n'.', n'cz.cuni.mff.odcleanstore.qualityassessment.impl.QualityAssessorImpl');
@@ -9,7 +15,6 @@ VALUES (n'Linker', n'The standard object identification transformer',  n'.', n'c
 INSERT INTO DB.ODCLEANSTORE.TRANSFORMERS (label, description, jarPath, fullClassName)
 VALUES (n'DN', n'The standard data normalization transformer', n'.', n'not yet known');
 
-DELETE FROM DB.ODCLEANSTORE.PIPELINES;
 
 INSERT INTO DB.ODCLEANSTORE.PIPELINES (label, description, runOnCleanDB)
 VALUES (n'Dirty', n'A basic dirty pipeline', 0);
@@ -17,20 +22,23 @@ VALUES (n'Dirty', n'A basic dirty pipeline', 0);
 INSERT INTO DB.ODCLEANSTORE.PIPELINES (label, description, runOnCleanDB)
 VALUES (n'Clean', n'A basic clean pipeline', 1);
 
-DELETE FROM DB.ODCLEANSTORE.TRANSFORMERS_TO_PIPELINES_ASSIGNMENT;
 
 INSERT INTO DB.ODCLEANSTORE.TRANSFORMERS_TO_PIPELINES_ASSIGNMENT (transformerId, pipelineId, workDirPath, configuration, priority)
-VALUES (1, 1, n'.', n'Config', 3);
+VALUES (1, 1, n'qa', n'', 1);
 
 INSERT INTO DB.ODCLEANSTORE.TRANSFORMERS_TO_PIPELINES_ASSIGNMENT (transformerId, pipelineId, workDirPath, configuration, priority)
-VALUES (2, 1, n'.', n'Config', 2);
+VALUES (2, 1, n'link', n'', 2);
 
 INSERT INTO DB.ODCLEANSTORE.TRANSFORMERS_TO_PIPELINES_ASSIGNMENT (transformerId, pipelineId, workDirPath, configuration, priority)
-VALUES (3, 1, n'.', n'Config', 1);
+VALUES (3, 1, n'.', n'', 3);
 
 INSERT INTO DB.ODCLEANSTORE.TRANSFORMERS_TO_PIPELINES_ASSIGNMENT (transformerId, pipelineId, workDirPath, configuration, priority)
-VALUES (1, 2, n'.', n'Config', 1);
+VALUES (1, 2, n'qa', n'', 1);
 
 INSERT INTO DB.ODCLEANSTORE.TRANSFORMERS_TO_PIPELINES_ASSIGNMENT (transformerId, pipelineId, workDirPath, configuration, priority)
-VALUES (2, 2, n'.', n'Config', 1);
+VALUES (2, 2, n'link', n'', 1);
+
+SELECT * FROM DB.ODCLEANSTORE.TRANSFORMERS;
+SELECT * FROM DB.ODCLEANSTORE.PIPELINES;
+SELECT * FROM DB.ODCLEANSTORE.TRANSFORMERS_TO_PIPELINES_ASSIGNMENT;
 
