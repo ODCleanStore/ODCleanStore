a3eb5fe704e0bb3b5d0f5c6128e38388b9507e8b
Jakub Daniel <jakub.daniel@gmail.com>
2012-06-26 14:18:23 +0200

    Data Normalization stub

---
+++ odcleanstore/backend/src/main/java/cz/cuni/mff/odcleanstore/datanormalization/exceptions/DataNormalizationException.java
@@ -0,0 +1,11 @@
+package cz.cuni.mff.odcleanstore.datanormalization.exceptions;
+
+import cz.cuni.mff.odcleanstore.shared.ODCleanStoreException;
+
+public class DataNormalizationException extends ODCleanStoreException {
+
+	public DataNormalizationException(String message) {
+		super(message);
+	}
+
+}
