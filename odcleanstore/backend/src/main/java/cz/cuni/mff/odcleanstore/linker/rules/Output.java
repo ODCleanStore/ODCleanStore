f67bf2eec0cb4c598965b1f3a638f3548a8b9a05
Tomas Soukup <supertom@email.cz>
2012-06-28 15:58:08 +0200

    OI rule value object implemented

---
+++ odcleanstore/backend/src/main/java/cz/cuni/mff/odcleanstore/linker/rules/Output.java
@@ -0,0 +1,18 @@
+package cz.cuni.mff.odcleanstore.linker.rules;
+
+public class Output {
+	Double minConfidence;
+	Double maxConfidence;
+	public Double getMinConfidence() {
+		return minConfidence;
+	}
+	public void setMinConfidence(Double minConfidence) {
+		this.minConfidence = minConfidence;
+	}
+	public Double getMaxConfidence() {
+		return maxConfidence;
+	}
+	public void setMaxConfidence(Double maxConfidence) {
+		this.maxConfidence = maxConfidence;
+	}
+}
---
+++ odcleanstore/backend/src/main/java/cz/cuni/mff/odcleanstore/linker/rules/SilkRule.java
@@ -0,0 +1,63 @@
+package cz.cuni.mff.odcleanstore.linker.rules;
+
+import java.util.List;
+
+public class SilkRule {
+	String label;
+	String linkType;
+	String sourceRestriction;
+	String targetRestriction;
+	String linkageRule;
+	Double filterThreshold;
+	Integer filterLimit;
+	List<Output> outputs;
+	
+	public String getLabel() {
+		return label;
+	}
+	public void setLabel(String label) {
+		this.label = label;
+	}
+	public String getLinkType() {
+		return linkType;
+	}
+	public void setLinkType(String linkType) {
+		this.linkType = linkType;
+	}
+	public String getSourceRestriction() {
+		return sourceRestriction;
+	}
+	public void setSourceRestriction(String sourceRestriction) {
+		this.sourceRestriction = sourceRestriction;
+	}
+	public String getTargetRestriction() {
+		return targetRestriction;
+	}
+	public void setTargetRestriction(String targetRestriction) {
+		this.targetRestriction = targetRestriction;
+	}
+	public String getLinkageRule() {
+		return linkageRule;
+	}
+	public void setLinkageRule(String linkageRule) {
+		this.linkageRule = linkageRule;
+	}
+	public Double getFilterThreshold() {
+		return filterThreshold;
+	}
+	public void setFilterThreshold(Double filterThreshold) {
+		this.filterThreshold = filterThreshold;
+	}
+	public Integer getFilterLimit() {
+		return filterLimit;
+	}
+	public void setFilterLimit(Integer filterLimit) {
+		this.filterLimit = filterLimit;
+	}
+	public List<Output> getOutputs() {
+		return outputs;
+	}
+	public void setOutputs(List<Output> outputs) {
+		this.outputs = outputs;
+	}
+}
