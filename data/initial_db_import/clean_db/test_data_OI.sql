e28e922b66fa5cb6bfdbd2fa896ad4e82c9b56a3
Tomas Soukup <supertom@email.cz>
2012-06-28 11:17:32 +0200

    OI rules DB schema extended

--- data/initial_db_import/clean_db/test_data_OI.sql
+++ data/initial_db_import/clean_db/test_data_OI.sql
@@ -2,26 +2,19 @@ DELETE FROM DB.ODCLEANSTORE.OI_RULES;
 DELETE FROM DB.ODCLEANSTORE.OI_RULES_GROUPS;
 
 INSERT INTO DB.ODCLEANSTORE.OI_RULES_GROUPS VALUES (1, n'pravidla pro linkovani testovacich dat o zakazkach od Tomase K.', n'pravidla pro linkovani testovacich dat o zakazkach od Tomase K.');
+    
+INSERT INTO DB.ODCLEANSTORE.OI_RULES VALUES (1, 1, n'title_supplier', n'owl:sameAs', 
+n'?x &lt;http://purl.org/procurement#supplier&gt; ?a .', n'?y &lt;http://purl.org/procurement#supplier&gt; ?b .',
+n'<LinkageRule>
+  <Compare weight="1" threshold="0.0" required="true" metric="equality" id="unnamed_3">
+    <Input path="?a/&lt;http://purl.org/procurement#title&gt;" id="unnamed_1"></Input>
+    <Input path="?b/&lt;http://purl.org/procurement#title&gt;" id="unnamed_2"></Input>
+  </Compare>
+</LinkageRule>',
+null, null
+);
 
-INSERT INTO DB.ODCLEANSTORE.OI_RULES VALUES (1, 1, n'<Interlink id="title_supplier">
-      <LinkType>owl:sameAs</LinkType>
-      <SourceDataset dataSource="Zakazky" var="a">
-        <RestrictTo>?x &lt;http://purl.org/procurement#supplier&gt; ?a .</RestrictTo>
-      </SourceDataset>
-      <TargetDataset dataSource="Zakazky" var="b">
-        <RestrictTo>?y &lt;http://purl.org/procurement#supplier&gt; ?b .</RestrictTo>
-      </TargetDataset>
-      <LinkageRule>
-        <Compare weight="1" threshold="0.0" required="true" metric="equality" id="unnamed_3">
-          <Input path="?a/&lt;http://purl.org/procurement#title&gt;" id="unnamed_1"></Input>
-          <Input path="?b/&lt;http://purl.org/procurement#title&gt;" id="unnamed_2"></Input>
-        </Compare>
-      </LinkageRule>
-      <Filter></Filter>
-      <Outputs>
-        <Output type="sparul" minConfidence="0.95" />
-      </Outputs>
-    </Interlink>');
+INSERT INTO DB.ODCLEANSTORE.OI_OUTPUTS VALUES (1, 1, 1, 0.95, null, null, null);
     
 SPARQL INSERT INTO <http://opendata.cz/data/namedGraph/2> {
 	<http://opendata.cz/data/767762e4-8180-428f-9c78-0edf60fcd911> <http://purl.org/procurement#supplier> <http://opendata.cz/data/0781c6a4-1147-473d-968a-666284ebc977> .
