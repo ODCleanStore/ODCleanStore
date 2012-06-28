0fcf08c7584da6f768071b0d840d0516485affba
Tomas Soukup <supertom@email.cz>
2012-06-28 16:17:09 +0200

    test updated for composite rules

--- odcleanstore/backend/src/test/java/cz/cuni/mff/odcleanstore/linker/impl/ConfigBuilderTest.java
+++ odcleanstore/backend/src/test/java/cz/cuni/mff/odcleanstore/linker/impl/ConfigBuilderTest.java
@@ -16,6 +16,8 @@ import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
 import cz.cuni.mff.odcleanstore.data.RDFprefix;
+import cz.cuni.mff.odcleanstore.linker.rules.Output;
+import cz.cuni.mff.odcleanstore.linker.rules.SilkRule;
 import cz.cuni.mff.odcleanstore.transformer.TransformationContext;
 import cz.cuni.mff.odcleanstore.transformer.TransformedGraph;
 import cz.cuni.mff.odcleanstore.transformer.TransformerException;
@@ -23,10 +25,24 @@ import cz.cuni.mff.odcleanstore.transformer.TransformerException;
 public class ConfigBuilderTest {
 	@Test
 	public void testCreateConfigFile() throws TransformerException, ParserConfigurationException, SAXException, IOException {
-		List<String> rules = new ArrayList<String>();
-		rules.add("<Interlink><SourceDataset/><TargetDataset/><LinkageRule><SomeContent/></LinkageRule></Interlink>");
-		rules.add("<Interlink><SourceDataset/><TargetDataset/><LinkageRule><OtherContent/></LinkageRule></Interlink>");
-		rules.add("<Interlink><SourceDataset/><TargetDataset/><LinkageRule><ThirdContent/></LinkageRule></Interlink>");
+		List<SilkRule> rules = new ArrayList<SilkRule>();
+		SilkRule rule = new SilkRule();
+		rule.setLabel("testRule");
+		rule.setLinkType("owl:sameAs");
+		rule.setSourceRestriction("?a rdf:type foo .");
+		rule.setTargetRestriction("?b rdf:type poo .");
+		rule.setFilterLimit(5);
+		rule.setFilterThreshold(0.9);
+		rule.setLinkageRule("<Interlink>ruleContent</Interlink>");
+		
+		List<Output> outputs = new ArrayList<Output>();
+		Output output = new Output();
+		output.setMinConfidence(0.95);
+		output.setMaxConfidence(0.98);
+		outputs.add(output);
+		rule.setOutputs(outputs);
+		
+		rules.add(rule);
 		
 		List<RDFprefix> prefixes = new ArrayList<RDFprefix>();
 		prefixes.add(new RDFprefix("rdf:", "http://www.w3.org/1999/02/22-rdf-syntax-ns#"));
@@ -38,13 +54,13 @@ public class ConfigBuilderTest {
 		
 		DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
 		
-		//File configFile = ConfigBuilder.createLinkConfigFile(rules, prefixes, graph, 
-		//		context, linksGraphName);
-		//Document configDoc = builder.parse(configFile);
+		File configFile = ConfigBuilder.createLinkConfigFile(rules, prefixes, graph, 
+				context, linksGraphName);
+		Document configDoc = builder.parse(configFile);
 		
-		//File expectedFile = new File("src/test/resources/expectedLinkConfig.xml");
-		//Document expectedDoc = builder.parse(expectedFile);
+		File expectedFile = new File("src/test/resources/expectedLinkConfig.xml");
+		Document expectedDoc = builder.parse(expectedFile);
 		
-		//assertTrue(configDoc.isEqualNode(expectedDoc));
+		assertTrue(configDoc.isEqualNode(expectedDoc));
 	}
 }
