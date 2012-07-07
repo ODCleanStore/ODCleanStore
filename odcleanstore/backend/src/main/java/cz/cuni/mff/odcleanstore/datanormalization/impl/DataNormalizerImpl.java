b8bcad8d112f0976ba7b9c155fc54be839963820
Jakub Daniel <jakub.daniel@gmail.com>
2012-07-07 17:38:04 +0200

    DN binding variables

--- odcleanstore/backend/src/main/java/cz/cuni/mff/odcleanstore/datanormalization/impl/DataNormalizerImpl.java
+++ odcleanstore/backend/src/main/java/cz/cuni/mff/odcleanstore/datanormalization/impl/DataNormalizerImpl.java
@@ -1,6 +1,10 @@
 package cz.cuni.mff.odcleanstore.datanormalization.impl;
 
 import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
@@ -8,15 +12,12 @@ import java.util.Iterator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import virtuoso.jena.driver.VirtGraph;
-
-import com.hp.hpl.jena.query.Syntax;
-import com.hp.hpl.jena.sparql.lang.UpdateParserFactory;
-import com.hp.hpl.jena.update.UpdateAction;
-import com.hp.hpl.jena.update.UpdateFactory;
-import com.hp.hpl.jena.update.UpdateRequest;
-
+import cz.cuni.mff.odcleanstore.connection.EnumLogLevel;
 import cz.cuni.mff.odcleanstore.connection.JDBCConnectionCredentials;
+import cz.cuni.mff.odcleanstore.connection.VirtuosoConnectionWrapper;
+import cz.cuni.mff.odcleanstore.connection.WrappedResultSet;
+import cz.cuni.mff.odcleanstore.connection.exceptions.ConnectionException;
+import cz.cuni.mff.odcleanstore.connection.exceptions.QueryException;
 import cz.cuni.mff.odcleanstore.datanormalization.DataNormalizer;
 import cz.cuni.mff.odcleanstore.datanormalization.exceptions.DataNormalizationException;
 import cz.cuni.mff.odcleanstore.datanormalization.rules.Rule;
@@ -26,13 +27,13 @@ import cz.cuni.mff.odcleanstore.transformer.TransformationContext;
 import cz.cuni.mff.odcleanstore.transformer.TransformedGraph;
 import cz.cuni.mff.odcleanstore.transformer.TransformedGraphException;
 import cz.cuni.mff.odcleanstore.transformer.TransformerException;
-import cz.cuni.mff.odcleanstore.vocabulary.ODCS;
 
 public class DataNormalizerImpl implements DataNormalizer {
 	
 	public static void main(String[] args) {
+
 		try {
-			for (int i = 1; i < 20 && i < 1844; ++i) {
+			for (int i = 1; i < 2 && i < 1844; ++i) {
 				final int id = i;
 
 				new DataNormalizerImpl().transformNewGraph(new TransformedGraph() {
@@ -114,10 +115,14 @@ public class DataNormalizerImpl implements DataNormalizer {
 					
 				});
 			}
-		} catch (TransformerException e) {
+		} catch (Exception e) {
 			System.err.println("DNMain: " + e.getMessage());
 		}
 	}
+	
+	private static final String selectFormat = "SPARQL SELECT %s FROM <%s> WHERE %s";
+	private static final String insertFormat = "SPARQL INSERT DATA INTO <%s> {%s}";
+	private static final String deleteFormat = "SPARQL DELETE DATA FROM <%s> {%s}";
 	
 	private static final Logger LOG = LoggerFactory.getLogger(DataNormalizerImpl.class);
 	
@@ -125,6 +130,29 @@ public class DataNormalizerImpl implements DataNormalizer {
 	private TransformationContext context;
 	
 	private Collection<Rule> rules;
+
+	/**
+	 * Connection to dirty database (needed in all cases to work on a new graph or a copy of an existing one)
+	 */
+	private VirtuosoConnectionWrapper dirtyConnection;
+
+	private VirtuosoConnectionWrapper getDirtyConnection () throws ConnectionException {
+        if (dirtyConnection == null) {
+        	dirtyConnection = VirtuosoConnectionWrapper.createConnection(context.getDirtyDatabaseCredentials());
+       	}
+		return dirtyConnection;
+	}
+
+	private void closeDirtyConnection() {
+		try {
+			if (dirtyConnection != null) {
+				dirtyConnection.close();
+			}
+		} catch (ConnectionException e) {
+		} finally {
+			dirtyConnection = null;
+		}
+	}
 
 	@Override
 	public void transformNewGraph(TransformedGraph inputGraph,
@@ -138,6 +166,8 @@ public class DataNormalizerImpl implements DataNormalizer {
 			applyRules();
 		} catch (DataNormalizationException e) {
 			throw new TransformerException(e);
+		} finally {
+			closeDirtyConnection();
 		}
 
 		LOG.info(String.format("Data Normalization applied to graph %s", inputGraph.getGraphName()));
@@ -153,33 +183,162 @@ public class DataNormalizerImpl implements DataNormalizer {
 		rules = new ArrayList<Rule>();
 		
 		rules.add(new Rule(
-				EnumRuleComponentType.RULE_COMPONENT_INSERT, "{<http://opendata.cz> <" + ODCS.score + "> ?score} WHERE {<http://opendata.cz> <" + ODCS.score + "> ?oldScore. BIND (replace(str(?oldScore), \"0\", \"1\") AS ?score)}"
+				EnumRuleComponentType.RULE_COMPONENT_INSERT, "<http://opendata.cz> <http://opendata.cz> \"bullshit\"", null, null,
+				EnumRuleComponentType.RULE_COMPONENT_INSERT, "?a ?b ?d", "?a ?b replace(str(?c), \"u\", \"*\") AS ?d", "{?a ?b ?c. FILTER (?c = \"bullshit\")}"
 				));
 	}
 
 	private void applyRules () throws DataNormalizationException {
-		Iterator<Rule> i = rules.iterator();
-		
-		while (i.hasNext()) {
-			Rule rule = i.next();
+		try {
+			getDirtyConnection();
+			
+			Iterator<Rule> i = rules.iterator();
+			
+			while (i.hasNext()) {
+				Rule rule = i.next();
 
-			String[] components = rule.getComponents();
-			
-			JDBCConnectionCredentials jdbc = context.getDirtyDatabaseCredentials();
-			
-			VirtGraph graph = new VirtGraph(inputGraph.getGraphName(),
-					jdbc.getConnectionString(),
-					jdbc.getUsername(),
-					jdbc.getPassword());
-			
-			for (int j = 0; j < components.length; ++j) {
-				UpdateRequest updateRequest = UpdateFactory.create(components[j], Syntax.syntaxSPARQL_11);
+				getDirtyConnection().adjustTransactionLevel(EnumLogLevel.TRANSACTION_LEVEL, false);
 				
-				System.err.println(components[j]);
+				performRule(rule);
 				
-				UpdateAction.execute(updateRequest, graph);
+				getDirtyConnection().commit();
+			}
+		} catch (ConnectionException e) {
+			throw new DataNormalizationException(e.getMessage());
+		} catch (QueryException e) {
+			throw new DataNormalizationException(e.getMessage());
+		} catch (SQLException e) {
+			throw new DataNormalizationException(e.getMessage());
+		}
+	}
+	
+	private void performRule (Rule rule) throws DataNormalizationException, ConnectionException, QueryException, SQLException {
+		Rule.Component[] components = rule.getComponents();
+
+		for (int j = 0; j < components.length; ++j) {
+			performRuleComponent(components[j]);
+		}
+	}
+
+	private void performRuleComponent (Rule.Component component) throws DataNormalizationException, ConnectionException, QueryException, SQLException {
+		if (component.getVariables() == null && component.getWhere() == null) {
+			performUpdate(component, null);
+		} else {
+			WrappedResultSet results = getDirtyConnection().executeSelect(
+				String.format(selectFormat, component.getVariables(), inputGraph.getGraphName(), component.getWhere()));
+			
+			while (results.next()) {
+				performUpdate(component, results.getCurrentResultSet());
 			}
 		}
+	}
+	private void performUpdate (Rule.Component component, ResultSet result) throws DataNormalizationException, ConnectionException, QueryException {
+		switch (component.getType()) {
+		case RULE_COMPONENT_INSERT:
+			String insert = String.format(insertFormat, inputGraph.getGraphName(), bindVariables(component.getTriples(), result));
+			
+			//System.err.println(insert);
+			
+			getDirtyConnection().execute(insert);
+			break;
+
+		case RULE_COMPONENT_DELETE:
+			String delete = String.format(deleteFormat, inputGraph.getGraphName(), bindVariables(component.getTriples(), result));
+			
+			//System.err.println(delete);
+			
+			getDirtyConnection().execute(delete);
+			break;
+		}
+	}
+	
+	private static String bindVariables (String triples, ResultSet result) throws DataNormalizationException {
+		
+		String bound = "";
+		
+		while (triples.length() > 0) {
+			int variable = findVariable(triples);
+			
+			if (variable < 0) {
+				bound += triples;
+
+				triples = "";
+			} else {
+				int length = findNonVariable(triples.substring(variable));
+				
+				String name = triples.substring(variable, variable + length);
+				
+				try {
+					String replacement;
+					
+					try {
+						replacement = "<" + new URI(result.getString(name)).toString() + ">";
+					} catch (URISyntaxException e) {
+						replacement = "\"" + result.getString(name) + "\"";
+					}
+
+					bound += triples.substring(0, variable - 1);
+					triples = triples.substring(variable + length);
+					bound += replacement;
+				} catch (SQLException e) {
+					throw new DataNormalizationException ("Unbound variable '" + name + "'");
+				}
+			}
+		}
+		
+		return bound;
+	}
+	
+	private static int findVariable (String triples) {
+		boolean quoted = false;
+		boolean url = false;
+		
+		for (int i = 0; i < triples.length(); ++i) {
+			switch (triples.charAt(i)) {
+			case '"':
+				if (!isEscaped(triples, i)) quoted = !quoted;
+				break;
+			case '<':
+				if (!quoted) url = true;
+				break;
+			case '>':
+				if (!quoted) url = false;
+				break;
+			case '?':
+				if (!quoted && !url) return i + 1;
+			}
+		}
+		
+		return -1;
+	}
+	
+	private static int findNonVariable (String triples) {
+		int i = 0;
+		
+		while (triples.length() > i &&
+				('a' <= triples.charAt(i) && triples.charAt(i) <= 'z' ||
+				'A' <= triples.charAt(i) && triples.charAt(i) <= 'Z' ||
+				'0' <= triples.charAt(i) && triples.charAt(i) <= '9' ||
+				triples.charAt(i) == '_')) {
+		
+			++i;
+		}
+		
+		return i;
+	}
+	
+	private static boolean isEscaped (String source, int position) {
+		int escapes = 0;
+		
+		while (position - 1 >= 0) {
+			
+			if (source.charAt(position - 1) == '\\') ++escapes;
+			else break;
+			
+			--position;
+		}
+		
+		return escapes % 2 == 1;
 	}
 
 	@Override
--- odcleanstore/backend/src/main/java/cz/cuni/mff/odcleanstore/datanormalization/rules/Rule.java
+++ odcleanstore/backend/src/main/java/cz/cuni/mff/odcleanstore/datanormalization/rules/Rule.java
@@ -16,45 +16,34 @@ public class Rule {
 		RULE_COMPONENT_DELETE
 	}
 	
-	public class Component implements Entry<EnumRuleComponentType, String> {
+	public class Component {
 		
-		public Component (EnumRuleComponentType type, String value) {
+		public Component (EnumRuleComponentType type, String triples, String variables, String where) {
 			this.type = type;
-			this.value = value;
+			this.triples = triples;
+			this.variables = variables;
+			this.where = where;
 		}
 		
 		private EnumRuleComponentType type;
-		private String value;
+		private String triples;
+		private String variables;
+		private String where;
 
-		@Override
-		public EnumRuleComponentType getKey() {
+		public EnumRuleComponentType getType () {
 			return type;
 		}
 
-		@Override
-		public String getValue() {
-			String output = "";
-
-			switch (type) {
-				case RULE_COMPONENT_INSERT:
-					output = "INSERT " + value;
-					break;
-
-				case RULE_COMPONENT_DELETE:
-					output = "DELETE " + value;
-					break;
-			}
-			
-			return output;
+		public String getTriples () {
+			return triples;
 		}
-
-		@Override
-		public String setValue(String value) {
-			String oldValue = this.value;
-			
-			this.value = value;
-			
-			return oldValue;
+		
+		public String getVariables () {
+			return variables;
+		}
+		
+		public String getWhere () {
+			return where;
 		}
 	}
 
@@ -63,25 +52,27 @@ public class Rule {
 	public Rule (Object... components) throws DataNormalizationException {
 		if (components.length % 2 == 1) throw new DataNormalizationException("Incomplete rule initialization list");
 		
-		for (int i = 0; i < components.length; i += 2) {
-			if (components[i] instanceof EnumRuleComponentType && components[i + 1] instanceof String) {
-				EnumRuleComponentType type = (EnumRuleComponentType)components[i];
-				String value = (String)components[i + 1];
+		for (int i = 0; i < components.length; i += 4) {
+			if (components[i] instanceof EnumRuleComponentType &&
+					components[i + 1] instanceof String &&
+					(components[i + 2] instanceof String || components[i + 2] == null) &&
+					(components[i + 2] instanceof String || components[i + 2] == null)) {
 
-				this.components.add(new Component(type, value));
+				EnumRuleComponentType type = (EnumRuleComponentType)components[i];
+				
+				String triples = (String)components[i + 1];
+				String variables = (String)components[i + 2];
+				String where = (String)components[i + 3];
+
+				this.components.add(new Component(type, triples, variables, where));
 			} else {
 				throw new DataNormalizationException("Invalid rule initialization list");
 			}
 		}
 	}
 	
-	public String[] getComponents () {
-		String[] rule = new String[components.size()];
+	public Component[] getComponents () {
 		
-		for (int i = 0; i < components.size(); ++i) {
-			rule[i] = components.elementAt(i).getValue();
-		}
-		
-		return rule;
+		return components.toArray(new Component[components.size()]);
 	}
 }
