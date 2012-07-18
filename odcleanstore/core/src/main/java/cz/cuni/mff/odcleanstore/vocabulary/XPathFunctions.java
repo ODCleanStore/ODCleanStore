c10a22ca4075751f37f8b0837a531f0c13e537e4
Jakub Daniel <jakub.daniel@gmail.com>
2012-07-18 14:32:25 +0200

    Add new vocabulary for functions

---
+++ odcleanstore/core/src/main/java/cz/cuni/mff/odcleanstore/vocabulary/XPathFunctions.java
@@ -0,0 +1,29 @@
+package cz.cuni.mff.odcleanstore.vocabulary;
+
+/**
+ * Vocabulary definitions for XPathFunctions.
+ *
+ * @author Jakub Daniel
+ */
+public final class XPathFunctions {
+    /** The namespaspace of the vocabulary as a string. */
+    private static final String NS = "http://www.w3.org/2005/xpath-functions:";
+
+    /**
+     * Returns the namespaspace of the vocabulary.
+     * @return namespace of the vocabulary as a string
+     */
+    public static String getURI() {
+        return NS;
+    }
+
+    /** Disable constructor for a utility class. */
+    private XPathFunctions() {
+    }
+
+    /* Vocabulary properties: */
+
+    // CHECKSTYLE:OFF
+    public static final String dateFunction = "http://www.w3.org/2005/xpath-functions:date";
+    // CHECKSTYLE:ON
+}
