d6e8aebde620c2a89d7d1f96833f76cb48046315
Tomas Soukup <supertom@email.cz>
2012-07-28 19:56:42 +0200

    test - problems with push

--- odcleanstore/backend/src/main/java/cz/cuni/mff/odcleanstore/linker/impl/LinkerImpl.java
+++ odcleanstore/backend/src/main/java/cz/cuni/mff/odcleanstore/linker/impl/LinkerImpl.java
@@ -7,9 +7,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
