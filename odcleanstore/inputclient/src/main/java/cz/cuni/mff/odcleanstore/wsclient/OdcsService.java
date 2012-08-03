ac516e293f9cc2d52978f990064ca1e9dca3f657
Petr Jerman <petr.jerman@centrum.cz>
2012-08-03 18:56:19 +0200

    InputClient - Metadata properties are typed

--- odcleanstore/inputclient/src/main/java/cz/cuni/mff/odcleanstore/wsclient/OdcsService.java
+++ odcleanstore/inputclient/src/main/java/cz/cuni/mff/odcleanstore/wsclient/OdcsService.java
@@ -1,6 +1,10 @@
 package cz.cuni.mff.odcleanstore.wsclient;
 
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 import cz.cuni.mff.odcleanstore.engine.inputws.InsertException_Exception;
 import cz.cuni.mff.odcleanstore.engine.inputws.InputWS;
@@ -20,7 +24,7 @@ public final class OdcsService {
 	/**
 	 * Create new odcs-inputclient webservice java client wrapper.
 	 * @param serviceLocation odcs-inputclient webservice location
-	 * @throws MalformedURLException 
+	 * @throws MalformedURLException serviceLocation URL format error
 	 */
 	public OdcsService(String serviceLocation) throws MalformedURLException {
 		_inputWSService = InputWSService.create(serviceLocation);
@@ -28,24 +32,24 @@ public final class OdcsService {
 	}
 
 	/**
-	 * Send insert message to odcs-inputclient SOAP webservice.
+	 * Send insert data message to odcs-inputclient SOAP webservice.
 	 * 
 	 * @param user odcs user for message
 	 * @param password odcs user password for message
 	 * @param metadata metadata asocciated with payload
 	 * @param payload payload in rdfxml or ttl format
-	 * @throws InsertException
+	 * @throws InsertException Exception returned from server
 	 */
 	public void insert(String user, String password, Metadata metadata, String payload) throws InsertException {
 
 		cz.cuni.mff.odcleanstore.engine.inputws.Metadata wsMetadata = new cz.cuni.mff.odcleanstore.engine.inputws.Metadata();
-
-		wsMetadata.setUuid(metadata.getUuid());
-		wsMetadata.getPublishedBy().addAll(metadata.getPublishedBy());
-		wsMetadata.getSource().addAll(metadata.getSource());
-		wsMetadata.getLicense().addAll(metadata.getLicense());
-		wsMetadata.setDataBaseUrl(metadata.getDataBaseUrl());
-		wsMetadata.setProvenanceBaseUrl(metadata.getProvenanceBaseUrl());
+	
+		wsMetadata.setUuid(convert(metadata.getUuid()));
+		wsMetadata.getPublishedBy().addAll(convert(metadata.getPublishedBy()));
+		wsMetadata.getSource().addAll(convert(metadata.getSource()));
+		wsMetadata.getLicense().addAll(convert(metadata.getLicense()));
+		wsMetadata.setDataBaseUrl(convert(metadata.getDataBaseUrl()));
+		wsMetadata.setProvenanceBaseUrl(convert(metadata.getProvenanceBaseUrl()));
 		wsMetadata.setProvenance(metadata.getProvenance());
 		wsMetadata.setPipelineName(metadata.getPipelineName());
 
@@ -54,5 +58,23 @@ public final class OdcsService {
 		} catch (InsertException_Exception e) {
 			throw new InsertException(e.getFaultInfo().getId(), e.getFaultInfo().getMessage(), e.getFaultInfo().getMoreInfo());
 		}
+	}
+	
+	private List<String> convert(List<URI> src) {
+		List<String> retVal= new ArrayList<String>();
+		for(URI item:src) {
+			if(item != null) {
+				retVal.add(item.toString());
+			}
+		}
+		return retVal;
+	}
+	
+	private String convert(URI src) {
+		return src != null ? src.toString() : null;
+	}
+
+	private String convert(UUID src) {
+		return src != null ? src.toString() : null;
 	}
 }
