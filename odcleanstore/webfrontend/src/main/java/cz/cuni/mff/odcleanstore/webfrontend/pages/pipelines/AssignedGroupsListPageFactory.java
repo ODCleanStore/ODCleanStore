a9fe024db448c68d10c78db285cf9e3a631785c0
Dusan Rychnovsky <dusan.rychnovsky@gmail.com>
2012-08-03 15:36:51 +0200

    WEB FRONTEND - allow setting rules' groups to DN tranformer instances

--- odcleanstore/webfrontend/src/main/java/cz/cuni/mff/odcleanstore/webfrontend/pages/pipelines/AssignedGroupsListPageFactory.java
+++ odcleanstore/webfrontend/src/main/java/cz/cuni/mff/odcleanstore/webfrontend/pages/pipelines/AssignedGroupsListPageFactory.java
@@ -1,12 +1,15 @@
 package cz.cuni.mff.odcleanstore.webfrontend.pages.pipelines;
 
 import cz.cuni.mff.odcleanstore.webfrontend.core.DaoLookupFactory;
+import cz.cuni.mff.odcleanstore.webfrontend.dao.dn.DNRulesGroupDao;
+import cz.cuni.mff.odcleanstore.webfrontend.dao.en.DNRuleAssignmentDao;
 import cz.cuni.mff.odcleanstore.webfrontend.dao.en.OIRuleAssignmentDao;
 import cz.cuni.mff.odcleanstore.webfrontend.dao.en.QARuleAssignmentDao;
 import cz.cuni.mff.odcleanstore.webfrontend.dao.oi.OIRulesGroupDao;
 import cz.cuni.mff.odcleanstore.webfrontend.dao.qa.QARulesGroupDao;
 import cz.cuni.mff.odcleanstore.webfrontend.pages.transformers.oi.OIGroupDetailPage;
 import cz.cuni.mff.odcleanstore.webfrontend.pages.transformers.qa.QAGroupDetailPage;
+import cz.cuni.mff.odcleanstore.webfrontend.pages.transformers.dn.DNGroupDetailPage;
 
 public class AssignedGroupsListPageFactory 
 {
@@ -33,6 +36,19 @@ public class AssignedGroupsListPageFactory
 			daoLookupFactory.getDaoForEntityWithSurrogateKey(OIRulesGroupDao.class), 
 			daoLookupFactory.getDaoForEntityWithSurrogateKey(OIRuleAssignmentDao.class), 
 			OIGroupDetailPage.class
+		);
+	}
+	
+	public static AssignedGroupsList createAssignedDNGroupsList(
+		DaoLookupFactory daoLookupFactory, Long transformerInstanceId
+	)
+	{
+		return new AssignedGroupsList(
+			"assignedGroupsListSection", 
+			transformerInstanceId, 
+			daoLookupFactory.getDaoForEntityWithSurrogateKey(DNRulesGroupDao.class), 
+			daoLookupFactory.getDaoForEntityWithSurrogateKey(DNRuleAssignmentDao.class), 
+			DNGroupDetailPage.class
 		);
 	}
 }
