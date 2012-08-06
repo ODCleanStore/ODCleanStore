c6b27abffe884f7b90cc542474f5f519ba2fd51f
Jakub Daniel <jakub.daniel@gmail.com>
2012-08-06 16:22:49 +0200

    Fix typo

--- odcleanstore/backend/src/main/java/cz/cuni/mff/odcleanstore/datanormalization/DataNormalizerFactory.java
+++ odcleanstore/backend/src/main/java/cz/cuni/mff/odcleanstore/datanormalization/DataNormalizerFactory.java
@@ -3,7 +3,7 @@ package cz.cuni.mff.odcleanstore.datanormalization;
 import cz.cuni.mff.odcleanstore.datanormalization.impl.DataNormalizerImpl;
 
 public class DataNormalizerFactory {
-	public static DataNormalizer createNormalizator (Integer groupId) {
+	public static DataNormalizer createNormalizer (Integer groupId) {
 		return new DataNormalizerImpl(groupId);
 	}
 }
