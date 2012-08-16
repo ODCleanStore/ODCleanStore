3ad78c8cad705c28ddb10356b21786e06ba58452
Dusan Rychnovsky <dusan.rychnovsky@gmail.com>
2012-08-16 12:53:07 +0200

    WEB FRONTEND - implement dependent-sortable-data-provider

---
+++ odcleanstore/webfrontend/src/main/java/cz/cuni/mff/odcleanstore/webfrontend/core/models/DependentSortableDataProvider.java
@@ -0,0 +1,89 @@
+package cz.cuni.mff.odcleanstore.webfrontend.core.models;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.wicket.extensions.markup.html.repeater.data.sort.SortOrder;
+import org.apache.wicket.extensions.markup.html.repeater.util.SortParam;
+import org.apache.wicket.extensions.markup.html.repeater.util.SortableDataProvider;
+import org.apache.wicket.model.IModel;
+
+import cz.cuni.mff.odcleanstore.webfrontend.bo.EntityWithSurrogateKey;
+import cz.cuni.mff.odcleanstore.webfrontend.dao.DaoForEntityWithSurrogateKey;
+import cz.cuni.mff.odcleanstore.webfrontend.dao.QueryCriteria;
+
+public class DependentSortableDataProvider<BO extends EntityWithSurrogateKey> extends SortableDataProvider<BO>
+{
+	private static final long serialVersionUID = 1L;
+	
+	private DaoForEntityWithSurrogateKey<BO> dao;
+	private List<BO> data;
+	private String columnName;
+	private Object value;
+	
+	/**
+	 * 
+	 * @param dao
+	 */
+	public DependentSortableDataProvider(DaoForEntityWithSurrogateKey<BO> dao, String columnName, Object value)
+	{
+		this(dao, "id", columnName, value);
+	}
+	
+	/**
+	 * 
+	 * @param dao
+	 */
+	public DependentSortableDataProvider(DaoForEntityWithSurrogateKey<BO> dao, String defaultSortColumnName,
+		String columnName, Object value)
+	{
+		setSort(defaultSortColumnName, SortOrder.ASCENDING);
+		
+		this.dao = dao;
+		this.columnName = columnName;
+		this.value = value;
+	}
+	
+	private List<BO> getData()
+	{
+		if (data == null)
+		{
+			SortParam sortParam = getSort();
+			
+			QueryCriteria criteria = new QueryCriteria();
+			criteria.addWhereClause(columnName, value);
+			criteria.addOrderByClause(sortParam.getProperty(), sortParam.isAscending());
+			
+			data = dao.loadAllBy(criteria);
+		}
+		
+		return data;
+	}
+	
+	@Override
+	public void detach()
+	{
+		data = null;
+	}
+	
+	public Iterator<? extends BO> iterator(int first, int count) 
+	{
+		// TODO: consider if it would be viable to fetch only the required data from DB here, instead of creating
+		// sublists
+		
+		return 
+			getData()
+				.subList(first, first + count)
+					.iterator();
+	}
+
+	public int size() 
+	{
+		return getData().size();
+	}
+
+	public IModel<BO> model(BO object) 
+	{
+		return new DetachableModel<BO>(dao, object);
+	}
+}
