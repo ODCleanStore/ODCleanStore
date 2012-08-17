51b39f940556a93e42a8d6f23029f556c3e96667
Jan Michelfeit <michelfeit.jan@gmail.com>
2012-08-17 11:55:16 +0200

    DOC - user manual

--- doc/build-user-manual.bat
+++ doc/build-user-manual.bat
@@ -1,3 +1,3 @@
-pdflatex.exe -quiet -interaction=nonstopmode "-job-name=ODCleanStore User Manual"content-user-manual.tex"
+pdflatex.exe -quiet -interaction=nonstopmode "-job-name=ODCleanStore User Manual" "content-user-manual.tex"
 pdflatex.exe -quiet -interaction=nonstopmode "-job-name=ODCleanStore User Manual" "content-user-manual.tex"
 pdflatex.exe -quiet -interaction=nonstopmode "-job-name=ODCleanStore User Manual" "content-user-manual.tex"
