5e29a4674b353d757a5017601ee47ef936322252
Dusan Rychnovsky <dusan.rychnovsky@gmail.com>
2012-08-23 09:46:44 +0200

    WEB FRONTEND - refactored router for greater readability

--- odcleanstore/webfrontend/src/main/java/cz/cuni/mff/odcleanstore/webfrontend/core/URLRouter.java
+++ odcleanstore/webfrontend/src/main/java/cz/cuni/mff/odcleanstore/webfrontend/core/URLRouter.java
@@ -12,29 +12,88 @@ import cz.cuni.mff.odcleanstore.webfrontend.pages.outputws.*;
 import cz.cuni.mff.odcleanstore.webfrontend.pages.prefixes.*;
 import cz.cuni.mff.odcleanstore.webfrontend.pages.ontologies.*;
 
+/**
+ * 
+ * @author Dusan
+ *
+ */
 public class URLRouter 
 {
 	private String webUrlPrefix;
 	
+	/**
+	 * 
+	 * @param webUrlPrefix
+	 */
 	public URLRouter(String webUrlPrefix)
 	{
 		this.webUrlPrefix = webUrlPrefix;
 	}
 	
+	/**
+	 * 
+	 * @param app
+	 */
 	public void setupRouting(WebApplication app)
 	{
+		setupLoginRoutes(app);
+		setupUserAccountsRoutes(app);
+		setupBackendRoutes(app);
+		setupOutputWSRoutes(app);
+		setupURLPrefixesRoutes(app);
+		setupOntologiesRoutes(app);
+	}
+
+	// 
+	// ------------------------------------------------------------------------
+	// LOGIN ROUTES
+	// ------------------------------------------------------------------------
+	//
+	
+	private void setupLoginRoutes(WebApplication app) 
+	{
 		app.mountPage(webUrlPrefix + "/login", LogInPage.class);
-		
+	}
+	
+	// 
+	// ------------------------------------------------------------------------
+	// USER ACCOUNTS ROUTES
+	// ------------------------------------------------------------------------
+	//
+	
+	private void setupUserAccountsRoutes(WebApplication app) 
+	{
 		app.mountPage(webUrlPrefix + "/user-accounts", UserAccountsPage.class);
 		app.mountPage(webUrlPrefix + "/user-accounts/list", AccountsListPage.class);
 		app.mountPage(webUrlPrefix + "/user-accounts/new", NewAccountPage.class);
-		app.mountPage(webUrlPrefix + "/user-accounts/edit", EditAccountPermissionsPage.class);
-		
+		app.mountPage(webUrlPrefix + "/user-accounts/edit", EditAccountPermissionsPage.class);	
+	}
+	
+	// 
+	// ------------------------------------------------------------------------
+	// BACKEND ROUTES
+	// ------------------------------------------------------------------------
+	//
+
+	private void setupBackendRoutes(WebApplication app) 
+	{
+		setupTransformersRoutes(app);
+		setupPipelinesRoutes(app);
+		setupOIRulesRoutes(app);
+		setupQARulesRoutes(app);
+		setupDNRulesRoutes(app);
+	}
+	
+	private void setupTransformersRoutes(WebApplication app) 
+	{
 		app.mountPage(webUrlPrefix + "/backend/transformers/list", TransformersListPage.class);
 		app.mountPage(webUrlPrefix + "/backend/transformers/new", NewTransformerPage.class);
 		app.mountPage(webUrlPrefix + "/backend/transformers/edit", EditTransformerPage.class);
 		app.mountPage(webUrlPrefix + "/backend/transformers/detail", TransformerDetailPage.class);
-		
+	}
+	
+	private void setupPipelinesRoutes(WebApplication app) 
+	{
 		app.mountPage(webUrlPrefix + "/backend/pipelines/list", PipelinesListPage.class);
 		app.mountPage(webUrlPrefix + "/backend/pipelines/new", NewPipelinePage.class);
 		app.mountPage(webUrlPrefix + "/backend/pipelines/edit", EditPipelinePage.class);
@@ -43,7 +102,10 @@ public class URLRouter
 		app.mountPage(webUrlPrefix + "/backend/pipelines/transformer-instances/edit", EditTransformerAssignmentPage.class);
 		app.mountPage(webUrlPrefix + "/backend/pipelines/transformer-instances/detail", TransformerInstanceDetailPage.class);
 		app.mountPage(webUrlPrefix + "/backend/pipelines/transformer-instances/assigned-groups/new", NewGroupAssignmentPage.class);
-		
+	}
+	
+	private void setupOIRulesRoutes(WebApplication app) 
+	{
 		app.mountPage(webUrlPrefix + "/backend/oi/groups/list", OIGroupsListPage.class);
 		app.mountPage(webUrlPrefix + "/backend/oi/groups/new", NewOIGroupPage.class);
 		app.mountPage(webUrlPrefix + "/backend/oi/groups/edit", EditOIGroupPage.class);
@@ -53,7 +115,10 @@ public class URLRouter
 		app.mountPage(webUrlPrefix + "/backend/oi/groups/rules/detail", OIRuleDetailPage.class);
 		app.mountPage(webUrlPrefix + "/backend/oi/groups/rules/db-outputs/new", NewDBOutputPage.class);
 		app.mountPage(webUrlPrefix + "/backend/oi/groups/rules/file-outputs/new", NewFileOutputPage.class);
+	}
 
+	private void setupQARulesRoutes(WebApplication app) 
+	{
 		app.mountPage(webUrlPrefix + "/backend/qa/groups/list", QAGroupsListPage.class);
 		app.mountPage(webUrlPrefix + "/backend/qa/groups/new", NewQAGroupPage.class);
 		app.mountPage(webUrlPrefix + "/backend/qa/groups/edit", EditQAGroupPage.class);
@@ -61,7 +126,10 @@ public class URLRouter
 		app.mountPage(webUrlPrefix + "/backend/qa/groups/rules/new", NewQARulePage.class);
 		app.mountPage(webUrlPrefix + "/backend/qa/groups/rules/edit", EditQARulePage.class);
 		app.mountPage(webUrlPrefix + "/backend/qa/groups/rules/detail", QARuleDetailPage.class);
+	}
 
+	private void setupDNRulesRoutes(WebApplication app) 
+	{
 		app.mountPage(webUrlPrefix + "/backend/dn/groups/list", DNGroupsListPage.class);
 		app.mountPage(webUrlPrefix + "/backend/dn/groups/new", NewDNGroupPage.class);
 		app.mountPage(webUrlPrefix + "/backend/dn/groups/edit", EditDNGroupPage.class);
@@ -72,18 +140,54 @@ public class URLRouter
 		app.mountPage(webUrlPrefix + "/backend/dn/groups/rules/components/new", NewDNRuleComponentPage.class);
 		app.mountPage(webUrlPrefix + "/backend/dn/groups/rules/components/edit", EditDNRuleComponentPage.class);
 		app.mountPage(webUrlPrefix + "/backend/dn/groups/rules/components/detail", DNRuleComponentDetailPage.class);
-		
+	}
+	
+	// 
+	// ------------------------------------------------------------------------
+	// OUTPUT-WS ROUTES
+	// ------------------------------------------------------------------------
+	//
+	
+	private void setupOutputWSRoutes(WebApplication app) 
+	{
+		setupAggregationPropertiesRoutes(app);
+		setupLabelPropertiesRoutes(app);
+	}
+
+	private void setupLabelPropertiesRoutes(WebApplication app) 
+	{
+		app.mountPage(webUrlPrefix + "/output-ws/label-properties/list", QELabelPropertiesListPage.class);
+		app.mountPage(webUrlPrefix + "/output-ws/label-properties/new", NewQELabelPropertyPage.class);	
+	}
+
+	private void setupAggregationPropertiesRoutes(WebApplication app) 
+	{
 		app.mountPage(webUrlPrefix + "/output-ws/aggregation-properties/list", CRPropertiesListPage.class);
 		app.mountPage(webUrlPrefix + "/output-ws/aggregation-properties/global/edit", EditGlobalAggregationSettingsPage.class);
 		app.mountPage(webUrlPrefix + "/output-ws/aggregation-properties/new", NewPropertyPage.class);
-		app.mountPage(webUrlPrefix + "/output-ws/aggregation-properties/edit", EditPropertyPage.class);
-		
-		app.mountPage(webUrlPrefix + "/output-ws/label-properties/list", QELabelPropertiesListPage.class);
-		app.mountPage(webUrlPrefix + "/output-ws/label-properties/new", NewQELabelPropertyPage.class);
+		app.mountPage(webUrlPrefix + "/output-ws/aggregation-properties/edit", EditPropertyPage.class);	
+	}
 
+	// 
+	// ------------------------------------------------------------------------
+	// URL-PREFIXES ROUTES
+	// ------------------------------------------------------------------------
+	//
+	
+	private void setupURLPrefixesRoutes(WebApplication app) 
+	{
 		app.mountPage(webUrlPrefix + "/url-prefixes/list", PrefixesListPage.class);
 		app.mountPage(webUrlPrefix + "/url-prefixes/new", NewPrefixPage.class);
-
+	}
+	
+	// 
+	// ------------------------------------------------------------------------
+	// ONTOLOGIES ROUTES
+	// ------------------------------------------------------------------------
+	//
+	
+	private void setupOntologiesRoutes(WebApplication app) 
+	{
 		app.mountPage(webUrlPrefix + "/ontologies/list", OntologiesListPage.class);
 		app.mountPage(webUrlPrefix + "/ontologies/new", NewOntologyPage.class);
 		app.mountPage(webUrlPrefix + "/ontologies/edit", EditOntologyPage.class);
