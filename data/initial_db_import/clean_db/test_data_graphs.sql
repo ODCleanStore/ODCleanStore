22e22d821f0abe73ec16709f0d5d67ad297ae36e
Dusan Rychnovsky <dusan.rychnovsky@gmail.com>
2012-09-05 19:20:15 +0200

    WEB FRONTEND - make the configuration of transformer instances field optional

---
+++ data/initial_db_import/clean_db/test_data_graphs.sql
@@ -0,0 +1,14 @@
+INSERT INTO DB.ODCLEANSTORE.EN_INPUT_GRAPHS (uuid, stateId, pipelineId, engineId, isInCleanDB)
+VALUES ('blabla01', 9, 1, 1, 1);
+
+INSERT INTO DB.ODCLEANSTORE.EN_INPUT_GRAPHS (uuid, stateId, pipelineId, engineId, isInCleanDB)
+VALUES ('blabla02', 9, 1, 1, 1);
+
+INSERT INTO DB.ODCLEANSTORE.EN_INPUT_GRAPHS (uuid, stateId, pipelineId, engineId, isInCleanDB)
+VALUES ('blabla01', 9, 2, 1, 1);
+
+INSERT INTO DB.ODCLEANSTORE.EN_INPUT_GRAPHS (uuid, stateId, pipelineId, engineId, isInCleanDB)
+VALUES ('blabla01', 11, 1, 1, 1);
+
+INSERT INTO DB.ODCLEANSTORE.EN_INPUT_GRAPHS (uuid, stateId, pipelineId, engineId, isInCleanDB)
+VALUES ('blabla01', 6, 1, 1, 1);
