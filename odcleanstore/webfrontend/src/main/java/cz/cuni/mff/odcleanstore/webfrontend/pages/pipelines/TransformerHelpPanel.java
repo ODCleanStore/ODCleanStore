404ff7ca67f2a00c3c26ef9faa6dac07d4d93b86
Dusan Rychnovsky <dusan.rychnovsky@gmail.com>
2012-09-06 11:29:41 +0200

    WEB FRONTEND - add help window skeletons to transformer related pages

---
+++ odcleanstore/webfrontend/src/main/java/cz/cuni/mff/odcleanstore/webfrontend/pages/pipelines/TransformerHelpPanel.java
@@ -0,0 +1,13 @@
+package cz.cuni.mff.odcleanstore.webfrontend.pages.pipelines;
+
+import org.apache.wicket.markup.html.panel.Panel;
+
+public class TransformerHelpPanel extends Panel
+{
+	private static final long serialVersionUID = 1L;
+
+	public TransformerHelpPanel(String compName) 
+	{
+		super(compName);
+	}
+}
