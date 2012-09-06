f22cece296c2d7d6898629c9ede7ee0ecc34ad3b
Dusan Rychnovsky <dusan.rychnovsky@gmail.com>
2012-09-06 14:06:15 +0200

    WEB FRONTEND - add help window skeletons to QA rules related pages

---
+++ odcleanstore/webfrontend/src/main/java/cz/cuni/mff/odcleanstore/webfrontend/pages/transformers/qa/QARuleHelpPanel.java
@@ -0,0 +1,13 @@
+package cz.cuni.mff.odcleanstore.webfrontend.pages.transformers.qa;
+
+import org.apache.wicket.markup.html.panel.Panel;
+
+public class QARuleHelpPanel extends Panel
+{
+	private static final long serialVersionUID = 1L;
+
+	public QARuleHelpPanel(String compName) 
+	{
+		super(compName);
+	}
+}
