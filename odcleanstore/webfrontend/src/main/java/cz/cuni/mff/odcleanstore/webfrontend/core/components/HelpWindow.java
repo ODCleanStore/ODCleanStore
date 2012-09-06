fc2d111fe4673fa34131f1bee0096f86b963a424
Dusan Rychnovsky <dusan.rychnovsky@gmail.com>
2012-09-06 10:37:37 +0200

    WEB FRONTEND - allow settings custom appearance for modal windows

--- odcleanstore/webfrontend/src/main/java/cz/cuni/mff/odcleanstore/webfrontend/core/components/HelpWindow.java
+++ odcleanstore/webfrontend/src/main/java/cz/cuni/mff/odcleanstore/webfrontend/core/components/HelpWindow.java
@@ -2,11 +2,15 @@ package cz.cuni.mff.odcleanstore.webfrontend.core.components;
 
 import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.request.resource.CssResourceReference;
+import org.apache.wicket.request.resource.ResourceReference;
 
 public class HelpWindow extends ModalWindow
 {
 	private static final long serialVersionUID = 1L;
 
+	private static ResourceReference CSS = new CssResourceReference(HelpWindow.class, "res/modal.css");
+	
 	private static final int INITIAL_WINDOW_WIDTH = 800;
 	private static final int INITIAL_WINDOW_HEIGHT = 600;
 	
@@ -18,5 +22,12 @@ public class HelpWindow extends ModalWindow
 		setInitialHeight(INITIAL_WINDOW_HEIGHT);
 		
 		setContent(content);
+	}
+	
+	@Override
+	protected ResourceReference newCssResource()
+	{
+		// return CSS;
+		return null;
 	}
 }
