932b7830f2a19590b5949ba470cfdf326096f44d
Tomas Soukup <supertom@email.cz>
2012-09-22 12:04:04 +0200

    odcs.ini path loaded from application.properties

--- odcleanstore/webfrontend/src/main/java/cz/cuni/mff/odcleanstore/webfrontend/core/ODCSWebFrontendApplication.java
+++ odcleanstore/webfrontend/src/main/java/cz/cuni/mff/odcleanstore/webfrontend/core/ODCSWebFrontendApplication.java
@@ -1,5 +1,10 @@
 package cz.cuni.mff.odcleanstore.webfrontend.core;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Properties;
+
 import org.apache.wicket.DefaultMapperContext;
 import org.apache.wicket.authroles.authentication.AbstractAuthenticatedWebSession;
 import org.apache.wicket.authroles.authentication.AuthenticatedWebApplication;
@@ -22,7 +27,8 @@ public class ODCSWebFrontendApplication extends AuthenticatedWebApplication
 {
 	private static final String WEB_URL_PREFIX = "odcs-web-frontend";
 	private static final String SPRING_CONFIG_LOCATION = "./config/spring.xml";
-	private static final String GLOBAL_CONFIG_LOCATION = "../../data/odcs_configuration/odcs.ini";
+	private static final String APP_PROPERTIES_LOCATION = "config/application.properties";
+	private static final String ODCS_PATH_PROPERTY = "odcs.config.path";
 
 	/** Spring context */
 	private ApplicationContext ctx;
@@ -58,10 +64,15 @@ public class ODCSWebFrontendApplication extends AuthenticatedWebApplication
 	{
 		super.init();
 		
+		String odcsConfigPath = null;
 		try {
-			ConfigLoader.loadConfig(GLOBAL_CONFIG_LOCATION);
+			Properties props = loadProperties();
+			odcsConfigPath = props.getProperty(ODCS_PATH_PROPERTY);
+			ConfigLoader.loadConfig(odcsConfigPath);
 		} catch (ConfigurationException e) {
-			throw new RuntimeException("Loading global config failed: " + GLOBAL_CONFIG_LOCATION);
+			throw new RuntimeException("Loading global config failed: " + odcsConfigPath);
+		} catch (IOException e) {
+			throw new RuntimeException("Loading application properties failed: " + APP_PROPERTIES_LOCATION);
 		}
 		
 		ctx = new ClassPathXmlApplicationContext(SPRING_CONFIG_LOCATION);
@@ -84,6 +95,15 @@ public class ODCSWebFrontendApplication extends AuthenticatedWebApplication
 				return WEB_URL_PREFIX; 
 			}
 		};
+	}
+	
+	private Properties loadProperties() throws IOException
+	{	
+		Properties props = new Properties();
+		InputStream inStream = Thread.currentThread().getContextClassLoader()
+				.getResourceAsStream(APP_PROPERTIES_LOCATION);
+        props.load(inStream);
+        return props;
 	}
 	
 	/**
