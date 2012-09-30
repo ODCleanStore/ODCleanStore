c22b3a0c5678b54cb611afc8b4fa1ba8cc0c8158
Jan Michelfeit <michelfeit.jan@gmail.com>
2012-09-30 22:01:40 +0200

    FE - merge dao refactoring

--- odcleanstore/webfrontend/src/main/java/cz/cuni/mff/odcleanstore/webfrontend/dao/en/AttachedEngineDao.java
+++ odcleanstore/webfrontend/src/main/java/cz/cuni/mff/odcleanstore/webfrontend/dao/en/AttachedEngineDao.java
@@ -1,53 +1,33 @@
 package cz.cuni.mff.odcleanstore.webfrontend.dao.en;
 
-import java.util.List;
-
 import org.springframework.jdbc.core.simple.ParameterizedRowMapper;
 
 import cz.cuni.mff.odcleanstore.webfrontend.bo.en.AttachedEngine;
 import cz.cuni.mff.odcleanstore.webfrontend.dao.DaoForEntityWithSurrogateKey;
-import cz.cuni.mff.odcleanstore.webfrontend.dao.QueryCriteria;
 
-public class AttachedEngineDao extends DaoForEntityWithSurrogateKey<AttachedEngine> {
-	//private static Logger logger = Logger.getLogger(OfficialPipelinesDao.class);
-	
+public class AttachedEngineDao extends DaoForEntityWithSurrogateKey<AttachedEngine>
+{
+	// private static Logger logger = Logger.getLogger(OfficialPipelinesDao.class);
+
 	private static final long serialVersionUID = 1L;
 	public static final String TABLE_NAME = TABLE_NAME_PREFIX + "EN_ATTACHED_ENGINES";
-	
+
 	private ParameterizedRowMapper<AttachedEngine> rowMapper;
-	
-	public AttachedEngineDao() {
+
+	public AttachedEngineDao()
+	{
 		rowMapper = new AttachedEngineRowMapper();
 	}
 
 	@Override
-	public String getTableName() {
+	public String getTableName()
+	{
 		return TABLE_NAME;
 	}
 
 	@Override
-	protected ParameterizedRowMapper<AttachedEngine> getRowMapper() {
+	protected ParameterizedRowMapper<AttachedEngine> getRowMapper()
+	{
 		return rowMapper;
 	}
-
-	@Override
-	public List<AttachedEngine> loadAll() {
-		String query =
-			"SELECT * FROM " + getTableName();		
-		return jdbcQuery(query, getRowMapper());
-	}
-	
-	@Override
-	public List<AttachedEngine> loadAllBy (QueryCriteria criteria) {
-
-		String query =
-			"SELECT * FROM " + getTableName() +
-			criteria.buildWhereClause() +
-			criteria.buildOrderByClause();
-
-		Object[] param = criteria.buildWhereClauseParams();
-		
-		return jdbcQuery(query, param, getRowMapper());
-	}
 }
-
