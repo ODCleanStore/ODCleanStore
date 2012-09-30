880e1258ba6a8032a3e4ef57285af036695e7b13
Dusan Rychnovsky <dusan.rychnovsky@gmail.com>
2012-09-30 15:52:54 +0200

    WEB FRONTEND - add javadoc comments to classes from the webfrontend.configuration package

--- odcleanstore/webfrontend/src/main/java/cz/cuni/mff/odcleanstore/webfrontend/configuration/Configuration.java
+++ odcleanstore/webfrontend/src/main/java/cz/cuni/mff/odcleanstore/webfrontend/configuration/Configuration.java
@@ -2,13 +2,33 @@ package cz.cuni.mff.odcleanstore.webfrontend.configuration;
 
 import cz.cuni.mff.odcleanstore.connection.JDBCConnectionCredentials;
 
+/**
+ * Application configuration class.
+ * 
+ * @author Dušan Rychnovský (dusan.rychnovsky@gmail.com)
+ *
+ */
 public class Configuration 
 {
+	/** connection credentials to the clean Virtuoso DB */
 	private JDBCConnectionCredentials cleanConnectionCoords;
-	private JDBCConnectionCredentials dirtyConnectionCoords;
-	private String gmailAddress;
-	private String gmailPassword;
 	
+	/** connection credentials to the dirty Virtuoso DB */
+	private JDBCConnectionCredentials dirtyConnectionCoords;
+	
+	/** the address of the GMAIL SMTP account */
+	private String gmailAddress;
+	
+	/** the password for the GMAIL SMTP account */
+	private String gmailPassword;
+
+	/**
+	 * 
+	 * @param cleanConnectionCoords
+	 * @param dirtyConnectionCoords
+	 * @param gmailAddress
+	 * @param gmailPassword
+	 */
 	public Configuration(
 		JDBCConnectionCredentials cleanConnectionCoords,
 		JDBCConnectionCredentials dirtyConnectionCoords,
@@ -20,22 +40,37 @@ public class Configuration
 		this.gmailPassword = gmailPassword;
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	public JDBCConnectionCredentials getCleanConnectionCoords() 
 	{
 		return cleanConnectionCoords;
 	}
 	
+	/**
+	 * 
+	 * @return
+	 */
 	public JDBCConnectionCredentials getDirtyConnectionCoords()
 	{
 		return dirtyConnectionCoords;
 	}
 
-
+	/**
+	 * 
+	 * @return
+	 */
 	public String getGmailAddress() 
 	{
 		return gmailAddress;
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	public String getGmailPassword() 
 	{
 		return gmailPassword;
