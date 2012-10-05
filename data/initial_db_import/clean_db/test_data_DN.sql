386018fa22adf5260fba0594482bb88bb0da38f6
Jan Michelfeit <michelfeit.jan@gmail.com>
2012-10-05 16:18:45 +0200

    DB SCHEMA - cleanup

--- data/initial_db_import/clean_db/test_data_DN.sql
+++ data/initial_db_import/clean_db/test_data_DN.sql
@@ -2,29 +2,27 @@
 INSERT INTO DB.ODCLEANSTORE.DN_RULES_GROUPS (label, description, authorId) VALUES (n'test group', n'this is a group for testing purposes', NULL);
 
 -- RULES
-INSERT INTO DB.ODCLEANSTORE.DN_RULES (id, groupId, description) VALUES (0, (SELECT id FROM DB.ODCLEANSTORE.DN_RULES_GROUPS WHERE label = 'test group'), '');
-INSERT INTO DB.ODCLEANSTORE.DN_RULES (id, groupId, description) VALUES (1, (SELECT id FROM DB.ODCLEANSTORE.DN_RULES_GROUPS WHERE label = 'test group'), '');
+INSERT INTO DB.ODCLEANSTORE.DN_RULES_UNCOMMITTED (id, groupId, description) VALUES (0, (SELECT id FROM DB.ODCLEANSTORE.DN_RULES_GROUPS WHERE label = 'test group'), '');
+INSERT INTO DB.ODCLEANSTORE.DN_RULES_UNCOMMITTED (id, groupId, description) VALUES (1, (SELECT id FROM DB.ODCLEANSTORE.DN_RULES_GROUPS WHERE label = 'test group'), '');
 
 -- COMPONENTS
-INSERT INTO DB.ODCLEANSTORE.DN_RULE_COMPONENTS (id, ruleId, typeId, modification, description) VALUES (
-	0,
+INSERT INTO DB.ODCLEANSTORE.DN_RULE_COMPONENTS_UNCOMMITTED (ruleId, typeId, modification, description) VALUES (
 	0,
 	(SELECT id FROM DB.ODCLEANSTORE.DN_RULE_COMPONENT_TYPES WHERE label = 'INSERT'),
 	'{?a ?b ?y} WHERE {GRAPH $$$$graph$$$$ {SELECT ?a ?b fn:replace(str(?c), ".", "*") AS ?y WHERE {?a ?b ?c}}}', 
 	'');
-INSERT INTO DB.ODCLEANSTORE.DN_RULE_COMPONENTS (id, ruleId, typeId, modification, description) VALUES (
-	1,
+INSERT INTO DB.ODCLEANSTORE.DN_RULE_COMPONENTS_UNCOMMITTED (ruleId, typeId, modification, description) VALUES (
 	0,
 	(SELECT id FROM DB.ODCLEANSTORE.DN_RULE_COMPONENT_TYPES WHERE label = 'DELETE'),
-	'{?a ?b ?c} WHERE {GRAPH $$$$graph$$$$ {?a ?b ?c} FILTER (contains(str(?c), "*") = false)}',
+	'{?a ?b ?c} WHERE {?a ?b ?c FILTER (contains(str(?c), "*") = false)}',
 	'');
-INSERT INTO DB.ODCLEANSTORE.DN_RULE_COMPONENTS (id, ruleId, typeId, modification, description) VALUES (
-	2,
+INSERT INTO DB.ODCLEANSTORE.DN_RULE_COMPONENTS_UNCOMMITTED (ruleId, typeId, modification, description) VALUES (
 	1,
 	(SELECT id FROM DB.ODCLEANSTORE.DN_RULE_COMPONENT_TYPES WHERE label = 'INSERT'),
-	'{?a <http://example.com/#test> ?b} WHERE {GRAPH $$$$graph$$$$ {?a ?b ?c} FILTER (contains(str(?c), "*******"))}', 
+	'{?a <http://example.com/#test> ?b} WHERE {?a ?b ?c FILTER (contains(str(?c), "*******"))}', 
 	'');
 
-
-INSERT INTO DB.ODCLEANSTORE.DN_RULES_UNCOMMITTED SELECT * FROM DB.ODCLEANSTORE.DN_RULES;
-INSERT INTO DB.ODCLEANSTORE.DN_RULE_COMPONENTS_UNCOMMITTED SELECT * FROM DB.ODCLEANSTORE.DN_RULE_COMPONENTS;
+DELETE FROM DB.ODCLEANSTORE.DN_RULES;
+INSERT INTO DB.ODCLEANSTORE.DN_RULES SELECT * FROM DB.ODCLEANSTORE.DN_RULES_UNCOMMITTED;
+DELETE FROM DB.ODCLEANSTORE.DN_RULE_COMPONENTS;
+INSERT INTO DB.ODCLEANSTORE.DN_RULE_COMPONENTS SELECT * FROM DB.ODCLEANSTORE.DN_RULE_COMPONENTS_UNCOMMITTED;
--- data/initial_db_import/clean_db/test_data_OI.sql
+++ data/initial_db_import/clean_db/test_data_OI.sql
@@ -1,11 +1,11 @@
-INSERT INTO DB.ODCLEANSTORE.OI_RULES_GROUPS (label, description) VALUES (n'zakazky TK', n'pravidla pro linkovani testovacich dat o zakazkach od Tomase K.');
+INSERT INTO DB.ODCLEANSTORE.OI_RULES_GROUPS (label, description) VALUES (n'test group', n'test OI group');
     
-INSERT INTO DB.ODCLEANSTORE.OI_RULES VALUES (
-	1,
-	(SELECT id FROM DB.ODCLEANSTORE.OI_RULES_GROUPS WHERE label = n'zakazky TK'),
-    n'title_supplier', 
+INSERT INTO DB.ODCLEANSTORE.OI_RULES_UNCOMMITTED (groupId, label, linkType, sourceRestriction, targetRestriction, linkageRule, filterThreshold, filterLimit) VALUES (
+	(SELECT id FROM DB.ODCLEANSTORE.OI_RULES_GROUPS WHERE label = n'test group'),
+    n'supplier', 
 	n'owl:sameAs', 
-	n'?x <http://purl.org/procurement#supplier> ?a .', n'?y <http://purl.org/procurement#supplier> ?b .',
+	n'?x <http://purl.org/procurement#supplier> ?a .', 
+	n'?y <http://purl.org/procurement#supplier> ?b .',
 	n'<LinkageRule>
   <Compare weight="1" threshold="0.0" required="true" metric="equality" id="unnamed_3">
     <Input path="?a/&lt;http://purl.org/procurement#title&gt;" id="unnamed_1"></Input>
@@ -16,16 +16,18 @@ INSERT INTO DB.ODCLEANSTORE.OI_RULES VALUES (
 	null
 );
 
-INSERT INTO DB.ODCLEANSTORE.OI_OUTPUTS VALUES (1, 1, (SELECT id FROM DB.ODCLEANSTORE.OI_OUTPUT_TYPES WHERE label = 'DB'), 0.95, null, null, null);
-INSERT INTO DB.ODCLEANSTORE.OI_OUTPUTS VALUES (2, 1, (SELECT id FROM DB.ODCLEANSTORE.OI_OUTPUT_TYPES WHERE label = 'FILE'), 0.95, null, 'C:/test/links.nt', (SELECT id FROM DB.ODCLEANSTORE.OI_FILE_FORMATS WHERE label = 'NTRIPLES'));
+INSERT INTO DB.ODCLEANSTORE.OI_OUTPUTS_UNCOMMITTED (ruleId, outputTypeId, minConfidence, maxConfidence, fileName, fileFormatId) VALUES (
+	(SELECT id FROM DB.ODCLEANSTORE.OI_RULES_UNCOMMITTED WHERE label = n'supplier'),
+	(SELECT id FROM DB.ODCLEANSTORE.OI_OUTPUT_TYPES WHERE label = 'DB'), 
+	0.95,
+	null, 
+	null,
+	null);
 
-INSERT INTO DB.ODCLEANSTORE.OI_RULES_UNCOMMITTED SELECT * FROM DB.ODCLEANSTORE.OI_RULES;
-INSERT INTO DB.ODCLEANSTORE.OI_OUTPUTS_UNCOMMITTED SELECT * FROM DB.ODCLEANSTORE.OI_OUTPUTS;
-    
-SPARQL INSERT INTO <http://opendata.cz/data/namedGraph/2> {
-	<http://opendata.cz/data/767762e4-8180-428f-9c78-0edf60fcd911> <http://purl.org/procurement#supplier> <http://opendata.cz/data/0781c6a4-1147-473d-968a-666284ebc977> .
-	<http://opendata.cz/data/0781c6a4-1147-473d-968a-666284ebc977> <http://purl.org/procurement#title> "ZAVOS s.r.o.".
-};
+DELETE FROM DB.ODCLEANSTORE.OI_RULES;
+INSERT INTO DB.ODCLEANSTORE.OI_RULES SELECT * FROM DB.ODCLEANSTORE.OI_RULES_UNCOMMITTED;
+DELETE FROM DB.ODCLEANSTORE.OI_OUTPUTS;
+INSERT INTO DB.ODCLEANSTORE.OI_OUTPUTS SELECT * FROM DB.ODCLEANSTORE.OI_OUTPUTS_UNCOMMITTED;
     
 /*INSERT INTO DB.DBA.SYS_XML_PERSISTENT_NS_DECL VALUES ('purl','http://purl.org/procurement#');*/
 
