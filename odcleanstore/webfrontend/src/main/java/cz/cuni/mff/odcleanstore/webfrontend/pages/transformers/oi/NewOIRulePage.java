3625a85c9ea76b6bf9de3292a272fb9802166672
Tomas Soukup <supertom@email.cz>
2012-10-14 19:12:04 +0200

    FE - OI rule import added to detail page as well

--- odcleanstore/webfrontend/src/main/java/cz/cuni/mff/odcleanstore/webfrontend/pages/transformers/oi/NewOIRulePage.java
+++ odcleanstore/webfrontend/src/main/java/cz/cuni/mff/odcleanstore/webfrontend/pages/transformers/oi/NewOIRulePage.java
@@ -1,15 +1,10 @@
 package cz.cuni.mff.odcleanstore.webfrontend.pages.transformers.oi;
 
-import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.TransformerException;
-
 import org.apache.log4j.Logger;
 import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
-import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.form.upload.FileUpload;
@@ -18,13 +13,10 @@ import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.util.ListModel;
 import org.apache.wicket.validation.validator.RangeValidator;
-import org.xml.sax.SAXException;
-import org.xml.sax.SAXParseException;
 
-import cz.cuni.mff.odcleanstore.linker.impl.ConfigBuilder;
-import cz.cuni.mff.odcleanstore.linker.rules.SilkRule;
 import cz.cuni.mff.odcleanstore.webfrontend.bo.Role;
 import cz.cuni.mff.odcleanstore.webfrontend.bo.oi.OIRule;
+import cz.cuni.mff.odcleanstore.webfrontend.core.components.OIRuleImportButton;
 import cz.cuni.mff.odcleanstore.webfrontend.core.components.RedirectWithParamButton;
 import cz.cuni.mff.odcleanstore.webfrontend.dao.exceptions.DaoException;
 import cz.cuni.mff.odcleanstore.webfrontend.dao.oi.OIRuleDao;
@@ -122,7 +114,7 @@ public class NewOIRulePage extends LimitedEditingPage
 		form.setMultiPart(true);
 		FileUploadField fileUpload = new FileUploadField("fileUpload", new ListModel<FileUpload>(new ArrayList<FileUpload>()));
 		form.add(fileUpload);
-		form.add(new ImportButton(fileUpload, form, "import"));
+		form.add(new OIRuleImportButton(fileUpload, form, "import"));
 		
 		add(form);
 	}
@@ -147,56 +139,5 @@ public class NewOIRulePage extends LimitedEditingPage
 		TextField<String> textfield = createTextfield("filterLimit", false);
 		textfield.add(new RangeValidator<Integer>(1, Integer.MAX_VALUE));
 		return textfield;
-	}
-	
-	private class ImportButton extends Button 
-	{
-		private static final long serialVersionUID = 1L;
-		
-		private FileUploadField fileUpload;
-		private Form<OIRule> form;
-		
-		public ImportButton(FileUploadField fileUpload, Form<OIRule> form, String compName)
-		{
-			super(compName);
-			this.fileUpload = fileUpload;
-			this.form = form;
-			setDefaultFormProcessing(false);
-		}
-		
-		@Override
-		public void onSubmit()
-		{
-			final FileUpload uploadedFile = fileUpload.getFileUpload();
-			if (uploadedFile != null)
-			{
-				try 
-				{
-					SilkRule silkRule = ConfigBuilder.parseRule(uploadedFile.getInputStream());
-					form.setModelObject(transformRule(silkRule));	
-					form.modelChanged();
-				}
-				catch (Exception e) 
-				{
-					logger.error(e.getMessage(), e);
-					getSession().error("Failed to parse the provided file.");
-				}
-			}
-		}
-		
-		private OIRule transformRule(SilkRule silkRule)
-		{
-			return  new OIRule(
-						null,
-						null,
-						silkRule.getLabel(),
-						silkRule.getLinkType(),
-						silkRule.getSourceRestriction(),
-						silkRule.getTargetRestriction(),
-						silkRule.getLinkageRule(),
-						silkRule.getFilterThreshold(),
-						silkRule.getFilterLimit()
-					);
-		}
 	}
 }
