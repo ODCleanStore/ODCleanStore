8400686d578513f3704e2441c93f0f569be8ad55
Dusan Rychnovsky <dusan.rychnovsky@gmail.com>
2012-10-21 10:34:20 +0200

    WEB FRONTEND - update javadoc comments in classes in the webfrontend.dao.onto package

--- odcleanstore/webfrontend/src/main/java/cz/cuni/mff/odcleanstore/webfrontend/dao/onto/OntologyMappingRowMapper.java
+++ odcleanstore/webfrontend/src/main/java/cz/cuni/mff/odcleanstore/webfrontend/dao/onto/OntologyMappingRowMapper.java
@@ -6,6 +6,12 @@ import java.sql.SQLException;
 import cz.cuni.mff.odcleanstore.webfrontend.bo.onto.RelationType;
 import cz.cuni.mff.odcleanstore.webfrontend.dao.CustomRowMapper;
 
+/**
+ * The Ontology mapping Row Mapper.
+ * 
+ * @author Tomáš Soukup
+ *
+ */
 public class OntologyMappingRowMapper extends CustomRowMapper<RelationType>
 {
 	private static final long serialVersionUID = 1L;
