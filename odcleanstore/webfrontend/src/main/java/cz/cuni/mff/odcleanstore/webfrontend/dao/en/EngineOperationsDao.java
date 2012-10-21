5c34e3e6af5c4e97468c8150f53468996e7df67a
Dusan Rychnovsky <dusan.rychnovsky@gmail.com>
2012-10-21 10:51:04 +0200

    WEB FRONTEND - update javadoc comments in classes in the webfrontend.dao.en package

--- odcleanstore/webfrontend/src/main/java/cz/cuni/mff/odcleanstore/webfrontend/dao/en/EngineOperationsDao.java
+++ odcleanstore/webfrontend/src/main/java/cz/cuni/mff/odcleanstore/webfrontend/dao/en/EngineOperationsDao.java
@@ -4,6 +4,13 @@ import org.apache.log4j.Logger;
 
 import cz.cuni.mff.odcleanstore.webfrontend.dao.Dao;
 
+/**
+ * A DAO which contains methods to signal the Engine to rerun certain
+ * graphs in the clean DB. 
+ * 
+ * @author Dušan Rychnovský (dusan.rychnovsky@gmail.com)
+ *
+ */
 public class EngineOperationsDao extends Dao
 {
 	private static final long serialVersionUID = 1L;
