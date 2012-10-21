4cd79f7590963a4ba52917ce728a471d2425c195
Dusan Rychnovsky <dusan.rychnovsky@gmail.com>
2012-10-21 10:44:30 +0200

    WEB FRONTEND - update javadoc comments in classes in the webfrontend.dao.exceptions package

--- odcleanstore/webfrontend/src/main/java/cz/cuni/mff/odcleanstore/webfrontend/dao/exceptions/DaoException.java
+++ odcleanstore/webfrontend/src/main/java/cz/cuni/mff/odcleanstore/webfrontend/dao/exceptions/DaoException.java
@@ -1,9 +1,19 @@
 package cz.cuni.mff.odcleanstore.webfrontend.dao.exceptions;
 
+/**
+ * An abstract parent of all application specific DAO exceptions.
+ * 
+ * @author Dušan Rychnovský (dusan.rychnovsky@gmail.com)
+ *
+ */
 public class DaoException extends Exception
 {
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * 
+	 * @param message
+	 */
 	public DaoException(String message)
 	{
 		super(message);
--- odcleanstore/webfrontend/src/main/java/cz/cuni/mff/odcleanstore/webfrontend/dao/exceptions/UniquenessViolationException.java
+++ odcleanstore/webfrontend/src/main/java/cz/cuni/mff/odcleanstore/webfrontend/dao/exceptions/UniquenessViolationException.java
@@ -1,9 +1,19 @@
 package cz.cuni.mff.odcleanstore.webfrontend.dao.exceptions;
 
+/**
+ * Exception thrown when a uniqueness constraint gets violated.
+ * 
+ * @author Dušan Rychnovský (dusan.rychnovsky@gmail.com)
+ *
+ */
 public class UniquenessViolationException extends DaoException
 {
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * 
+	 * @param attributeName
+	 */
 	public UniquenessViolationException(String attributeName)
 	{
 		super(
