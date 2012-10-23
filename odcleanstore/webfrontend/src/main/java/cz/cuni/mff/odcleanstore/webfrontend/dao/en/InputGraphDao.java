e82103c9ae6719371778ffc057b2d358693c356a
Jan Michelfeit <michelfeit.jan@gmail.com>
2012-10-23 10:32:22 +0200

    FE - dummy example of dumping graph in InputGraphDao

--- odcleanstore/webfrontend/src/main/java/cz/cuni/mff/odcleanstore/webfrontend/dao/en/InputGraphDao.java
+++ odcleanstore/webfrontend/src/main/java/cz/cuni/mff/odcleanstore/webfrontend/dao/en/InputGraphDao.java
@@ -11,7 +11,8 @@ import cz.cuni.mff.odcleanstore.vocabulary.ODCSInternal;
 import cz.cuni.mff.odcleanstore.webfrontend.bo.en.InputGraph;
 import cz.cuni.mff.odcleanstore.webfrontend.dao.DaoForEntityWithSurrogateKey;
 
-public class InputGraphDao extends DaoForEntityWithSurrogateKey<InputGraph> {
+public class InputGraphDao extends DaoForEntityWithSurrogateKey<InputGraph>
+{
 
 	private static final long serialVersionUID = 1L;
 	public static final String GRAPHS_IN_ERROR_TABLE_NAME = TABLE_NAME_PREFIX + "EN_GRAPHS_IN_ERROR";
@@ -22,76 +23,126 @@ public class InputGraphDao extends DaoForEntityWithSurrogateKey<InputGraph> {
 	public static final String ATTACHED_ENGINES_TABLE_NAME = TABLE_NAME_PREFIX + "EN_ATTACHED_ENGINES";
 
 	private ParameterizedRowMapper<InputGraph> rowMapper;
-	
-	public InputGraphDao() {
+
+	public InputGraphDao()
+	{
 		rowMapper = new InputGraphRowMapper();
 	}
-	
+
 	@Override
 	public InputGraph load(Integer id)
 	{
 		return loadBy("iGraph.id", id);
 	}
-	
+
 	@Override
-	public String getTableName() {
+	public String getTableName()
+	{
 		return INPUT_GRAPHS_TABLE_NAME;
 	}
 
 	@Override
-	protected ParameterizedRowMapper<InputGraph> getRowMapper() {
+	protected ParameterizedRowMapper<InputGraph> getRowMapper()
+	{
 		return rowMapper;
 	}
-	
+
 	@Override
 	protected String getSelectAndFromClause()
 	{
 		String query =
 			"SELECT " +
-			"iGraph.id AS id, " +
-			"iGraph.engineId AS engineId, " +
-			"iGraph.pipelineId AS pipelineId, " +
-			"iGraph.uuid AS uuid, " +
-			"iGraph.stateId AS stateId, " +
-			"iGraph.isInCleanDB AS isInCleanDB, " +
-			"iGraph.updated AS updated, " +
-			"engine.uuid AS engineUuid, " +
-			"pipeline.label AS pipelineLabel, " +
-			"iState.label AS stateLabel " +
-			"FROM " +
-			INPUT_GRAPHS_TABLE_NAME + " AS iGraph JOIN " +
-			INPUT_GRAPHS_STATES_TABLE_NAME + " AS iState ON iGraph.stateId = iState.id JOIN " +
-			PIPELINES_TABLE_NAME + " AS pipeline ON pipeline.id = iGraph.pipelineId JOIN " +
-			ATTACHED_ENGINES_TABLE_NAME + " AS engine ON engine.id = iGraph.engineId ";
+				"iGraph.id AS id, " +
+				"iGraph.engineId AS engineId, " +
+				"iGraph.pipelineId AS pipelineId, " +
+				"iGraph.uuid AS uuid, " +
+				"iGraph.stateId AS stateId, " +
+				"iGraph.isInCleanDB AS isInCleanDB, " +
+				"iGraph.updated AS updated, " +
+				"engine.uuid AS engineUuid, " +
+				"pipeline.label AS pipelineLabel, " +
+				"iState.label AS stateLabel " +
+				"FROM " +
+				INPUT_GRAPHS_TABLE_NAME + " AS iGraph JOIN " +
+				INPUT_GRAPHS_STATES_TABLE_NAME + " AS iState ON iGraph.stateId = iState.id JOIN " +
+				PIPELINES_TABLE_NAME + " AS pipeline ON pipeline.id = iGraph.pipelineId JOIN " +
+				ATTACHED_ENGINES_TABLE_NAME + " AS engine ON engine.id = iGraph.engineId ";
 		return query;
 	}
-	
-	public String getContent(Integer graphId) throws Exception {
+
+	public String getContent(Integer graphId) throws Exception
+	{
 		InputGraph inputGraph = load(graphId);
 		String graphName = ODCSInternal.dataGraphUriPrefix + inputGraph.UUID;
-		
+
 		VirtuosoConnectionWrapper connection = null;
 		File tmpFile = null;
 		try
 		{
-			
+
 			connection = createVirtuosoConnectionWrapper(EnumDatabaseInstance.CLEAN);
 			tmpFile = GraphLoaderUtils.getImportExportTmpFile(connection, EnumDatabaseInstance.CLEAN);
 			connection.exportToTTL(tmpFile, graphName);
-			
+
 			// TODO
-		} 
+		}
 		finally
 		{
 			if (connection != null)
 			{
 				connection.closeQuietly();
 			}
-			if (tmpFile != null) 
+			if (tmpFile != null)
 			{
-				tmpFile.delete();
+				tmpFile.delete(); // ?
 			}
 		}
-		return null;
+		return "";
 	}
+
+	/*
+	private String getDirName(EnumDatabaseInstance instance) {
+		EngineConfig config = ConfigLoader.getConfig().getEngineGroup();
+
+		return instance == EnumDatabaseInstance.CLEAN ? config.getCleanImportExportDir() : config.getDirtyImportExportDir();
+	}
+	
+	private File getDumpFile(InputGraph inputGraph) {
+		File file = new File(getDirName(inputGraph.isInCleanDB ? EnumDatabaseInstance.CLEAN : EnumDatabaseInstance.DIRTY), inputGraph.UUID + "-" + UUID.randomUUID());
+
+		file.deleteOnExit();
+
+		return file;
+	}
+
+	public String getContent(Integer graphId) throws Exception {
+		InputGraph inputGraph = load(graphId);
+		
+		String query =
+				"CALL dump_graph_ttl(?, ?)";
+		
+		File file = getDumpFile(inputGraph);
+
+		String filename = file.getAbsolutePath().replace("\\", "/");
+		
+		try {
+			Object[] param = { ODCSInternal.dataGraphUriPrefix + inputGraph.UUID, filename };
+		
+			jdbcUpdate(query, param);
+			
+			StringBuilder content = new StringBuilder();
+			
+			for (String line : Files.readAllLines(file.toPath(), Charset.defaultCharset())) {
+				content.append(line);
+				content.append("\n");
+			}
+			
+			return content.toString();
+		} finally {
+			file.delete();
+		}
+		
+		
+	}
+	*/
 }
