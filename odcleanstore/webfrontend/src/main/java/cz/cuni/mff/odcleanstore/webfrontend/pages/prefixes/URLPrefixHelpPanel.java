3ba9d77b1716164738b971475f95b008905ac1c2
Dusan Rychnovsky <dusan.rychnovsky@gmail.com>
2012-10-28 10:46:36 +0100

    WEB FRONTEND - add javadoc comments to classes which belong to the webfrontend.pages.prefixes package

--- odcleanstore/webfrontend/src/main/java/cz/cuni/mff/odcleanstore/webfrontend/pages/prefixes/URLPrefixHelpPanel.java
+++ odcleanstore/webfrontend/src/main/java/cz/cuni/mff/odcleanstore/webfrontend/pages/prefixes/URLPrefixHelpPanel.java
@@ -5,11 +5,21 @@ import org.apache.wicket.markup.html.panel.Panel;
 
 import cz.cuni.mff.odcleanstore.webfrontend.bo.Role;
 
+/**
+ * URL-prefix help panel.
+ * 
+ * @author Dušan Rychnovský (dusan.rychnovsky@gmail.com)
+ *
+ */
 @AuthorizeInstantiation({ Role.ADM })
 public class URLPrefixHelpPanel extends Panel
 {
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * 
+	 * @param compName
+	 */
 	public URLPrefixHelpPanel(String compName) 
 	{
 		super(compName);
