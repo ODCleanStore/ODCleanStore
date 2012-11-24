c018013eb658fa62c20c1d36a0ac82cff2555e56
Jakub Daniel <jakub.daniel@gmail.com>
2012-11-24 21:06:44 +0100

    Backend - Fix Boolean conversion DN rule

--- odcleanstore/backend/src/main/java/cz/cuni/mff/odcleanstore/datanormalization/rules/DataNormalizationBooleanRule.java
+++ odcleanstore/backend/src/main/java/cz/cuni/mff/odcleanstore/datanormalization/rules/DataNormalizationBooleanRule.java
@@ -6,7 +6,6 @@ import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.vocabulary.XSD;
 
 import cz.cuni.mff.odcleanstore.datanormalization.exceptions.DataNormalizationException;
-import cz.cuni.mff.odcleanstore.vocabulary.XPathFunctions;
 
 public class DataNormalizationBooleanRule extends DataNormalizationRule {
 	private static final long serialVersionUID = 1L;
@@ -14,8 +13,7 @@ public class DataNormalizationBooleanRule extends DataNormalizationRule {
 	private static final String boolTruePattern = "?o = '1' OR lcase(str(?o)) = 'true' OR lcase(str(?o)) = 'yes' OR lcase(str(?o)) = 't' OR lcase(str(?o)) = 'y'";
 	private static final String boolFalsePattern = "?o = '0' OR lcase(str(?o)) = 'false' OR lcase(str(?o)) = 'no' OR lcase(str(?o)) = 'f' OR lcase(str(?o)) = 'n'";
 	
-	private static final String convertedTruePropertyValueFormat = "DELETE {?s <%s> ?o} INSERT {?s <%s> ?t} WHERE {GRAPH $$graph$$ {SELECT ?s <%s>(1) AS ?t ?o WHERE {?s <%s> ?o. FILTER (" + boolTruePattern + ")}}}";
-	private static final String convertedFalsePropertyValueFormat = "DELETE {?s <%s> ?o} INSERT {?s <%s> ?f} WHERE {GRAPH $$graph$$ {SELECT ?s <%s>(0) AS ?f ?o WHERE {?s <%s> ?o. FILTER (" + boolFalsePattern + ")}}}";
+	private static final String convertedTruePropertyValueFormat = "DELETE {?s <%s> ?o} INSERT {?s <%s> ?t} WHERE {GRAPH $$graph$$ {SELECT ?s (if(" + boolTruePattern + ", 1, 0)) AS ?t ?o WHERE {?s <%s> ?o. FILTER (" + boolTruePattern + " OR " + boolFalsePattern + ")}}}";
 
 	public DataNormalizationBooleanRule (Integer id, Integer groupId, Resource property) throws DataNormalizationException {
 		super(id, groupId,
@@ -23,11 +21,7 @@ public class DataNormalizationBooleanRule extends DataNormalizationRule {
 				"Convert " + property.getLocalName() + " into " + XSD.xboolean.getLocalName(),
 				
 				"MODIFY",
-				String.format(Locale.ROOT, convertedTruePropertyValueFormat, property.getURI(), property.getURI(), XPathFunctions.boolFunction, property.getURI()),
-				"Convert " + XSD.xboolean.getLocalName() + " value for the property " + property.getURI() + " (\"1\", \"true\", ...)",
-
-				"MODIFY",
-				String.format(Locale.ROOT, convertedFalsePropertyValueFormat, property.getURI(), property.getURI(), XPathFunctions.boolFunction, property.getURI()),
-				"Convert " + XSD.xboolean.getLocalName() + " value for the property " + property.getURI() + " (\"0\", \"false\", ...)");
+				String.format(Locale.ROOT, convertedTruePropertyValueFormat, property.getURI(), property.getURI(), property.getURI()),
+				"Convert " + XSD.xboolean.getLocalName() + " value for the property " + property.getURI() + " (\"1\", \"true\", ...)");
 	}
 }
