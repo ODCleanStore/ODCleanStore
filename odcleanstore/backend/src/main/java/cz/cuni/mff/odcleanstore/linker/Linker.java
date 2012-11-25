6a65eaa233a041f2cf9bf81b89e25cf0dcd040e5
Tomas Soukup <supertom@email.cz>
2012-11-25 17:59:18 +0100

    OI - javadoc completed, visibility modifiers corrected

--- odcleanstore/backend/src/main/java/cz/cuni/mff/odcleanstore/linker/Linker.java
+++ odcleanstore/backend/src/main/java/cz/cuni/mff/odcleanstore/linker/Linker.java
@@ -19,8 +19,30 @@ import cz.cuni.mff.odcleanstore.transformer.TransformerException;
  * @author Tomas Soukup
  */
 public interface Linker extends Transformer {
+	/**
+	 * 
+	 * Debugs linkage rules.
+	 * 
+	 * @param input RDF data, supported languages are RDF/XML, N3 and its subsets
+	 * @param context emulates real context used when running in a pipeline; 
+	 * transformer directory and database credentials are needed
+	 * @param tableVersion determines, whether to use uncommitted or committed version of rules
+	 * @return list of debug results, one for each rules
+	 * @throws TransformerException
+	 */
 	public List<DebugResult> debugRules(String input, TransformationContext context, TableVersion tableVersion)
 			throws TransformerException;
+	/**
+	 * Debugs linkage rules.
+	 * 
+	 * @param inputFile input RDF data, file is passed to Silk without any processing
+	 * @param context emulates real context used when running in a pipeline; 
+	 * transformer directory and database credentials are needed
+	 * @param tableVersion determines, whether to use uncommitted or committed version of rules
+	 * @param language format of the input data, supported languages are RDF/XML, N3 and its subsets
+	 * @return list of debug results, one for each rules
+	 * @throws TransformerException
+	 */
 	public List<DebugResult> debugRules(File inputFile, TransformationContext context, TableVersion tableVersion,
 			SerializationLanguage language) throws TransformerException;
 }
