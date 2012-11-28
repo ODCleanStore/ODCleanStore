d61291cdc9b730ea2cb0d2105572d6dfd854b97b
Tomas Soukup <supertom@email.cz>
2012-11-28 20:23:58 +0100

    OI - javadoc completed

--- odcleanstore/backend/src/main/java/cz/cuni/mff/odcleanstore/linker/rules/FileOutput.java
+++ odcleanstore/backend/src/main/java/cz/cuni/mff/odcleanstore/linker/rules/FileOutput.java
@@ -1,17 +1,35 @@
 package cz.cuni.mff.odcleanstore.linker.rules;
 
+/**
+ * Business entity representing file output in linkage rule.
+ * @author Tomas Soukup
+ */
 public class FileOutput extends Output {
+	
 	String name;
 	String format;
+	
+	/**
+	 * @return output filename
+	 */
 	public String getName() {
 		return name;
 	}
+	/**
+	 * @param name output filename
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
+	/**
+	 * @return output file format
+	 */
 	public String getFormat() {
 		return format;
 	}
+	/**
+	 * @param format output file format
+	 */
 	public void setFormat(String format) {
 		this.format = format;
 	}
--- odcleanstore/backend/src/main/java/cz/cuni/mff/odcleanstore/linker/rules/SilkRule.java
+++ odcleanstore/backend/src/main/java/cz/cuni/mff/odcleanstore/linker/rules/SilkRule.java
@@ -3,6 +3,10 @@ package cz.cuni.mff.odcleanstore.linker.rules;
 import java.math.BigDecimal;
 import java.util.List;
 
+/**
+ * Business entity representing linkage rule for Silk.
+ * @author Tomas Soukup
+ */
 public class SilkRule {
 	Integer id;
 	String label;
@@ -14,57 +18,111 @@ public class SilkRule {
 	Integer filterLimit;
 	List<Output> outputs;
 	
+	/**
+	 * @return rule ID
+	 */
 	public Integer getId() {
 		return id;
 	}
+	/**
+	 * @param id rule ID
+	 */
 	public void setId(Integer id) {
 		this.id = id;
 	}
+	/**
+	 * @return rule label
+	 */
 	public String getLabel() {
 		return label;
 	}
+	/**
+	 * @param label rule label
+	 */
 	public void setLabel(String label) {
 		this.label = label;
 	}
+	/**
+	 * @return link type
+	 */
 	public String getLinkType() {
 		return linkType;
 	}
+	/**
+	 * @param linkType link type
+	 */
 	public void setLinkType(String linkType) {
 		this.linkType = linkType;
 	}
+	/**
+	 * @return source restriction
+	 */
 	public String getSourceRestriction() {
 		return sourceRestriction;
 	}
+	/**
+	 * @param sourceRestriction source restriction
+	 */
 	public void setSourceRestriction(String sourceRestriction) {
 		this.sourceRestriction = sourceRestriction;
 	}
+	/**
+	 * @return target restriction
+	 */
 	public String getTargetRestriction() {
 		return targetRestriction;
 	}
+	/**
+	 * @param targetRestriction target restriction
+	 */
 	public void setTargetRestriction(String targetRestriction) {
 		this.targetRestriction = targetRestriction;
 	}
+	/**
+	 * @return linkage rule XML fragment
+	 */
 	public String getLinkageRule() {
 		return linkageRule;
 	}
+	/**
+	 * @param linkageRule linkage rule XML fragment
+	 */
 	public void setLinkageRule(String linkageRule) {
 		this.linkageRule = linkageRule;
 	}
+	/**
+	 * @return filter threshold
+	 */
 	public BigDecimal getFilterThreshold() {
 		return filterThreshold;
 	}
+	/**
+	 * @param filterThreshold filter threshold
+	 */
 	public void setFilterThreshold(BigDecimal filterThreshold) {
 		this.filterThreshold = filterThreshold;
 	}
+	/**
+	 * @return filter limit
+	 */
 	public Integer getFilterLimit() {
 		return filterLimit;
 	}
+	/**
+	 * @param filterLimit filter limit
+	 */
 	public void setFilterLimit(Integer filterLimit) {
 		this.filterLimit = filterLimit;
 	}
+	/**
+	 * @return list of rule outputs
+	 */
 	public List<Output> getOutputs() {
 		return outputs;
 	}
+	/**
+	 * @param outputs list of rule outputs
+	 */
 	public void setOutputs(List<Output> outputs) {
 		this.outputs = outputs;
 	}
