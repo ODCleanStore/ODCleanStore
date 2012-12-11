98d3541bc4312bc633c9b355d1473e597bcbde16
Jan Michelfeit <michelfeit.jan@gmail.com>
2012-12-11 13:38:55 +0100

    DN - fix bif: prefixes in generated rules

--- odcleanstore/backend/src/main/java/cz/cuni/mff/odcleanstore/datanormalization/rules/DataNormalizationBooleanRule.java
+++ odcleanstore/backend/src/main/java/cz/cuni/mff/odcleanstore/datanormalization/rules/DataNormalizationBooleanRule.java
@@ -1,32 +1,32 @@
 package cz.cuni.mff.odcleanstore.datanormalization.rules;
 
-import java.util.Locale;
+import cz.cuni.mff.odcleanstore.datanormalization.exceptions.DataNormalizationException;
 
 import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.vocabulary.XSD;
 
-import cz.cuni.mff.odcleanstore.datanormalization.exceptions.DataNormalizationException;
+import java.util.Locale;
 
 /**
  * A rule used by Data Normalization Rule Generation from Ontologies
- * 
+ *
  * replaces common expressions used to denote logical true / false: 0, 1, t, f, true, false, y, n, yes, no
- * 
+ *
  * @author Jakub Daniel
  */
 public class DataNormalizationBooleanRule extends DataNormalizationRule {
 	private static final long serialVersionUID = 1L;
-	
-	private static final String boolTruePattern = "?o = '1' OR lcase(str(?o)) = 'true' OR lcase(str(?o)) = 'yes' OR lcase(str(?o)) = 't' OR lcase(str(?o)) = 'y'";
-	private static final String boolFalsePattern = "?o = '0' OR lcase(str(?o)) = 'false' OR lcase(str(?o)) = 'no' OR lcase(str(?o)) = 'f' OR lcase(str(?o)) = 'n'";
-	
+
+	private static final String boolTruePattern = "?o = '1' OR bif:lcase(str(?o)) = 'true' OR bif:lcase(str(?o)) = 'yes' OR bif:lcase(str(?o)) = 't' OR bif:lcase(str(?o)) = 'y'";
+	private static final String boolFalsePattern = "?o = '0' OR bif:lcase(str(?o)) = 'false' OR bif:lcase(str(?o)) = 'no' OR bif:lcase(str(?o)) = 'f' OR bif:lcase(str(?o)) = 'n'";
+
 	private static final String convertedTruePropertyValueFormat = "DELETE {?s <%s> ?o} INSERT {?s <%s> ?t} WHERE {GRAPH $$graph$$ {SELECT ?s (if(" + boolTruePattern + ", 1, 0)) AS ?t ?o WHERE {?s <%s> ?o. FILTER (" + boolTruePattern + " OR " + boolFalsePattern + ")}}}";
 
 	public DataNormalizationBooleanRule (Integer id, Integer groupId, Resource property) throws DataNormalizationException {
 		super(id, groupId,
 				property.getLocalName() + "-bool-conversion",
 				"Convert " + property.getLocalName() + " into " + XSD.xboolean.getLocalName(),
-				
+
 				"MODIFY",
 				String.format(Locale.ROOT, convertedTruePropertyValueFormat, property.getURI(), property.getURI(), property.getURI()),
 				"Convert " + XSD.xboolean.getLocalName() + " value for the property " + property.getURI() + " (\"1\", \"true\", ...)");
--- odcleanstore/backend/src/main/java/cz/cuni/mff/odcleanstore/datanormalization/rules/DataNormalizationNumberRule.java
+++ odcleanstore/backend/src/main/java/cz/cuni/mff/odcleanstore/datanormalization/rules/DataNormalizationNumberRule.java
@@ -1,30 +1,30 @@
 package cz.cuni.mff.odcleanstore.datanormalization.rules;
 
-import java.util.Locale;
+import cz.cuni.mff.odcleanstore.datanormalization.exceptions.DataNormalizationException;
 
 import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.vocabulary.XSD;
 
-import cz.cuni.mff.odcleanstore.datanormalization.exceptions.DataNormalizationException;
+import java.util.Locale;
 
 /**
  * A rule used by Data Normalization Rule Generation from Ontologies
- * 
+ *
  * converts anything to a number
  * a bit to complicated but allows its argument to be incompatible -> result = 0
- * 
+ *
  * @author Jakub Daniel
  */
 public class DataNormalizationNumberRule extends DataNormalizationRule {
 	private static final long serialVersionUID = 1L;
-	
-	private static final String convertedNumberPropertyValueFormat = "DELETE {?s <%s> ?o} INSERT {?s <%s> ?x} WHERE {GRAPH $$graph$$ {SELECT ?s (bif:sign(fn:number(str(?o))) * bif:__max_notnull(abs(fn:number(str(?o))), 0)) AS ?x ?o WHERE {?s <%s> ?o}}}";
+
+	private static final String convertedNumberPropertyValueFormat = "DELETE {?s <%s> ?o} INSERT {?s <%s> ?x} WHERE {GRAPH $$graph$$ {SELECT ?s (bif:sign(fn:number(str(?o))) * bif:__max_notnull(bif:abs(fn:number(str(?o))), 0)) AS ?x ?o WHERE {?s <%s> ?o}}}";
 
 	public DataNormalizationNumberRule(Integer id, Integer groupId, Resource property) throws DataNormalizationException {
 		super(id, groupId,
 				property.getLocalName() + "-number-conversion",
 				"Convert " + property.getLocalName() + " into " + XSD.xdouble.getLocalName(),
-				
+
 				"MODIFY",
 				String.format(Locale.ROOT, convertedNumberPropertyValueFormat, property.getURI(), property.getURI(), property.getURI()),
 				"Convert " + XSD.xdouble.getLocalName() + " value for the property " + property.getURI());
