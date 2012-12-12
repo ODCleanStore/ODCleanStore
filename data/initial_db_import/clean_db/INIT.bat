574f4f118ef2ae7d6ce31fba2eb262a33bdad9d1
Dusan Rychnovsky <dusan.rychnovsky@gmail.com>
2012-12-12 09:35:03 +0100

    DB SCHEMA - add a batch script to initialize the structure of the clean DB

---
+++ data/initial_db_import/clean_db/INIT.bat
@@ -0,0 +1,23 @@
+:: Initializes the clean DB structure.
+::
+
+@ECHO OFF
+
+IF %1.==. GOTO ERR
+IF %2.==. GOTO ERR
+IF %3.==. GOTO ERR
+IF %4.==. GOTO ERR
+
+isql %1:%2 %3 %4 00_autocommit_on.sql
+isql %1:%2 %3 %4 01_create_rel_database.sql
+isql %1:%2 %3 %4 02_stored_procedures.sql
+isql %1:%2 %3 %4 03_odcs_prefixes.sql
+isql %1:%2 %3 %4 04_user_accounts.sql
+isql %1:%2 %3 %4 05_enable_fulltext_index.sql
+
+GOTO END
+
+:ERR
+ECHO USAGE: ALL.bat host port username password
+
+:END
