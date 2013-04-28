085cf00bf2baf7306b226cf55e70fbae40a1231f
Jan Michelfeit <michelfeit.jan@gmail.com>
2013-04-28 20:47:28 +0200

    CR - ODCSSourceConfidenceCalculator considers more publishers

--- odcleanstore/conflictresolution/src/main/java/cz/cuni/mff/odcleanstore/conflictresolution/confidence/impl/ODCSSourceConfidenceCalculator.java
+++ odcleanstore/conflictresolution/src/main/java/cz/cuni/mff/odcleanstore/conflictresolution/confidence/impl/ODCSSourceConfidenceCalculator.java
@@ -2,7 +2,9 @@ package cz.cuni.mff.odcleanstore.conflictresolution.confidence.impl;
 
 import org.openrdf.model.Model;
 import org.openrdf.model.Resource;
+import org.openrdf.model.Statement;
 import org.openrdf.model.URI;
+import org.openrdf.model.Value;
 import org.openrdf.model.impl.ValueFactoryImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,8 +15,8 @@ import cz.cuni.mff.odcleanstore.vocabulary.ODCS;
 /**
  * @author Jan Michelfeit
  */
-public class _ODCSSourceConfidenceCalculator implements SourceConfidenceCalculator {
-    private static final Logger LOG = LoggerFactory.getLogger(_ODCSSourceConfidenceCalculator.class);
+public class ODCSSourceConfidenceCalculator implements SourceConfidenceCalculator {
+    private static final Logger LOG = LoggerFactory.getLogger(ODCSSourceConfidenceCalculator.class);
     private static final URI sourceScoreProperty = ValueFactoryImpl.getInstance().createURI(ODCS.score);
     private static final URI publisherScoreProperty = ValueFactoryImpl.getInstance().createURI(ODCS.publisherScore);
     private static final URI publisherProperty = ValueFactoryImpl.getInstance().createURI(ODCS.publishedBy);
@@ -22,7 +24,7 @@ public class _ODCSSourceConfidenceCalculator implements SourceConfidenceCalculat
     protected final double defaultScore;
     protected final double publisherScoreWeight;
 
-    public _ODCSSourceConfidenceCalculator(double defaultScore, double publisherScoreWeight) {
+    public ODCSSourceConfidenceCalculator(double defaultScore, double publisherScoreWeight) {
         this.publisherScoreWeight = publisherScoreWeight;
         this.defaultScore = defaultScore;
     }
@@ -40,15 +42,7 @@ public class _ODCSSourceConfidenceCalculator implements SourceConfidenceCalculat
             sourceScore = null;
         }
 
-        Double publisherScore = null;
-        try {
-            Resource publisher = metadata.filter(source, publisherProperty, null).objectResource();
-            if (publisher != null) {
-                publisherScore = metadata.filter(publisher, publisherScoreProperty, null).objectLiteral().doubleValue();
-            }
-        } catch (Exception e) { // ModelException, NumbefFormatException
-            publisherScore = null;
-        }
+        Double publisherScore = getAveragePublisherScore(metadata, source);
 
         if (publisherScore != null && sourceScore != null) {
             return publisherScoreWeight * publisherScore + (1 - publisherScoreWeight) * sourceScore;
@@ -60,5 +54,29 @@ public class _ODCSSourceConfidenceCalculator implements SourceConfidenceCalculat
             LOG.debug("No source or publisher score present, using default score for source {}", source);
             return defaultScore;
         }
+    }
+    
+    private double getAveragePublisherScore(Model metadata, Resource source) {
+        double averageScore = 0;
+        int publishersCount = 0;
+        
+        Model publishers = metadata.filter(source, publisherProperty, null);
+        for (Statement statement : publishers) {
+            try {
+                Value publisher = statement.getObject();
+                if (!(publisher instanceof Resource)) {
+                    continue;
+                }
+                averageScore += metadata.filter((Resource) publisher, publisherScoreProperty, null)
+                        .objectLiteral().doubleValue();
+                publishersCount++;
+            } catch (Exception e) { // ModelException, NumbefFormatException
+                continue;
+            }
+        }
+
+        return publishersCount > 0
+                ? averageScore / publishersCount
+                : null;
     }
 }
